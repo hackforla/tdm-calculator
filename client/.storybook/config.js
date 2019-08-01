fe6071d293dc5a926cb0c1a84e1ad3018d682c32
John Darragh <darragh@entrotech.net>
2019-07-31 22:22:50 -0700

    Set up storyshot snapshot testing for react components and Jest unit tests of calculation engine

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+import "../src/styles/index.scss";
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../src", true, /\.stories\.js$/);
+const req = requireContext("../src/components", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
---
+++ client/src/test-data/tdm-calc-examples.js
@@ -0,0 +1,18 @@
+export const project1 = {
+  SF_RETAIL: 4000,
+  SF_RESTAURANT: 3000
+};
+
+export const project2 = {
+  SF_RETAIL: 19500,
+  SF_RESTAURANT: 30500
+};
+
+export const project3 = {
+  UNITS_HABIT_LT3: 50,
+  UNITS_HABIT_3: 40,
+  UNITS_HABIT_GT3: 30,
+  UNITS_SR_INDEPENDENT: 6,
+  UNITS_SKILLED_NURSING: 12,
+  UNITS_DEMENTIA: 4
+};
