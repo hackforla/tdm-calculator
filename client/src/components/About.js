36089063ec429c119cf28424eac6000860a7d07c
Nicholas Pung <nicholaspung>
2019-08-02 10:07:41 -0700

    Added routing to project

---
+++ client/src/components/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+    return <>About</>
+}
+
+export default About;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDom from "react-dom";
 import TdmCalculationContainer from "./components/TdmCalculation";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Admin from "./components/Admin";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./styles/index.scss";
 
@@ -12,10 +16,20 @@ class App extends React.Component {
       <div>
         <Header />
         <NavBar />
-        <TdmCalculationContainer />
+
+        <Route exact path="/" component={TdmCalculationContainer} />
+        <Route path="/calculation" component={TdmCalculationContainer} />
+        <Route path="/about" component={About} />
+        <Route path="/contactus" component={ContactUs} />
+        <Route path="/admin" component={Admin} />
       </div>
     );
   }
 }
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(
+  <Router>
+    <App />
+  </Router>,
+  document.querySelector("#root")
+);
