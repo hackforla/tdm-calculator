8822dc2327818e28c21b77c9b3adbaf664b05373
Fang Yi Liu <fangyiliu@gmail.com>
2019-08-06 17:20:58 -0700

    Copy some rule viewer code into Admin

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,7 +1,26 @@
 import React from "react";
+import RuleViews from "./RuleViews";
 
-const Admin = () => {
-    return <>Admin</>
+// TODO move retrieval of rules to App.js
+
+const Admin = (props) => {
+  const { rules, onInputChange, resultRuleCodes } = props;
+  const calculationRules =
+    rules &&
+    rules.filter(rule => rule.category === "calculation" && rule.used);
+  return (
+    <div>
+      <h3>Admin</h3>
+      <div>
+        <h4>Calculation Rules</h4>
+        {calculationRules && calculationRules.length > 0 ? (
+          <RuleViews rules={calculationRules} onInputChange={onInputChange} />
+        ) : (
+          <div>No Rules Loaded</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
