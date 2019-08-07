a97d6ec90e5e7674a9fb6702e13b66bc0ea53915
Fang Yi Liu <fangyiliu@gmail.com>
2019-08-06 22:15:36 -0700

    Load rules and display select dropdown

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as ruleService from "../services/rule.service";
 import RuleViews from "./RuleViews";
 
-// TODO move retrieval of rules to App.js
+const calculationId = 1;
 
-const Admin = (props) => {
-  const { rules, onInputChange, resultRuleCodes } = props;
+const Admin = props => {
+  const [rules, setRules] = useState([]);
+
+  function handleRulesChange(rules) {
+    setRules(rules);
+  }
+
+  useEffect(() => {
+    ruleService
+      .getByCalculationId(calculationId)
+      .then(response => {
+        //console.log(response.data);
+        handleRulesChange(response.data);
+      })
+      .catch(err => {
+        console.log(JSON.stringify(err, null, 2));
+      });
+  }, []);
+
+  const { onInputChange } = props;
   const calculationRules =
-    rules &&
-    rules.filter(rule => rule.category === "calculation" && rule.used);
+    rules && rules.filter(rule => rule.category === "calculation");
+
+  //console.log(rules);
+  //console.log(calculationRules);
+
   return (
     <div>
       <h3>Admin</h3>
@@ -21,6 +43,6 @@ const Admin = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default Admin;
