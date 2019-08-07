990fbf7d4c76e42540a09c354e439d5f854e2fb2
Fang Yi Liu <fangyiliu@gmail.com>
2019-08-07 07:33:39 -0700

    Remove all RuleView and display only the selected one

--- client/src/components/RuleViewContainer.js
+++ client/src/components/RuleViewContainer.js
@@ -4,10 +4,14 @@ import RuleView from "./RuleView";
 const RuleViewContainer = props => {
   const { rules } = props;
   const [ruleId, setRuleId] = useState(rules[0].id);
+  const rule = rules.filter(rule => rule.id === ruleId)[0];
 
   return (
     <React.Fragment>
-      <select value={ruleId} onChange={e => setRuleId(e.target.value)}>
+      <select
+        value={ruleId}
+        onChange={e => setRuleId(parseInt(e.target.value))}
+      >
         {rules && rules.length > 0
           ? rules.map(rule => (
               <option key={rule.id} value={rule.id}>
@@ -16,13 +20,7 @@ const RuleViewContainer = props => {
             ))
           : null}
       </select>
-      {rules && rules.length > 0
-        ? rules.map(rule => (
-            <div key={rule.id}>
-              <RuleView rule={rule} onInputChange={props.onInputChange} />
-            </div>
-          ))
-        : null}
+      <RuleView rule={rule} onInputChange={props.onInputChange} />
     </React.Fragment>
   );
 };
