edb047760496f4b8d206d22807bed3853dfdaef1
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-08-12 19:19:07 -0700

    added basic route, service, controller for login

--- app/services/account.service.js
+++ app/services/account.service.js
@@ -1,96 +1,76 @@
 const mssql = require("../../mssql");
+const TYPES = require("tedious").TYPES;
 const bcrypt = require('bcrypt')
 
-const promise = (resolvedItem) => {
+const promise = (itemToResolve) => {
     return new Promise(function(resolve, reject) {
-        resolve(resolvedItem);
+        resolve(itemToResolve);
     });
 }
 
-// testing
+
+const postLogin = (req, res) => {
+    const tempUser = {
+        email: "claire@tdm.com",
+        // the original string password is: "tdmpassword"
+        password: "$2b$10$44otJ0IfAbSMM.E1h3XqVu8KVKOuy13ANcgN3X4kCamnS0svttyH6"
+    }
+
+
+    return promise({'login message': 'you hit postLogin() in account.service'})
+}
+
+
+
+const postRegister = (req, res) => {
+    return bcrypt.hash(req.password, 10)
+        .then( hash => {
+        // TODO: 1) check if user is in db 
+        
+        
+        // TODO: 2) create user and add to db
+
+        // NOT SURE HOW TO MAKE REQUESTS TO THE DATABASE...
+
+        // mssql.executeProc("Create_Account")
+        // .then(sqlRequest => {
+        //     sqlRequest.addParamenter("email", TYPES.VarChar, req.email, {length: 255})
+        //     sqlRequest.addParamenter("password", TYPES.VarChar, hash, {length: 72})
+        //     sqlRequest.addOutputParameter("id", TYPES.Int, null)
+        // })
+        // .then(response => {
+        //     console.log(response)
+        //     return response.outputParameters;
+        // })
+        // .then(res => console.log(res))
+        // .catch(err => {
+        //     console.log(err)
+        //     res.status(500).json(err)
+        // })
+    
+
+        // TEMP JSON RESPONSE HARD CODED
+        const userInfo = {
+            
+            email: req.email,
+            password: hash, 
+            message: "TEMP JSON DATA BACK. NEEDS TO BE REPLACED WITH THE USER ID and TOKEN"
+        }
+        return userInfo
+    })
+}
+
+
+// testing 
 const getMessage = (res) => {
     return  promise(
         {message: "welcome to the account routes"}
     )
 }
 
-const postRegister = (req, res) => {
-    return bcrypt.hash(req.password, 10)
-        .then( hash => {
-        // TODO: 1) check if user is in db 2) create user
-
-        //TODO: need to return the user id and token, temporarily just returning user back
-        const userInfo = {
-                    email: req.email,
-                    hash
-                }
-        return user
-    })
-}
-
-
-
-
-
-// return  res.json({
-        //                 message: "successfully posted to register",
-        //                 user
-        //             })
-            // if (err) {
-            //     // if error, we coudln't hash the password and couldn't store it
-            //     return res.status(500).json({
-            //         error: err
-            //     })
-            // } else {
-            //     // if no error, create user with the hashed password
-            //     // TODO: add details on saving user here
-            //     const user = {
-            //         email: req.email,
-            //         hash: hash
-            //     }
-                
-            //     return res.status(201).json({
-            //         message: "successfully posted to register",
-            //         user
-            //     })
-            // }
-
-   // TODO: add valication, if user exists in the system already...
-
-//     // if user is not in db, then has the password
-
-    
-
-
-    // bcrypt.hash(req.password, 10, (err, hash) => {
-    //     if (err) {
-    //         // if error, we coudln't hash the password and couldn't store it
-    //         return res.status(500).json({
-    //             error: err
-    //         })
-    //     } else {
-    //         // if no error, create user with the hashed password
-    //         // TODO: add details on saving user here
-    //         const user = {
-    //             email: req.email,
-    //             hash: hash
-    //         }
-            
-    //         return res.status(201).json({
-    //             message: "successfully posted to register",
-    //             user
-    //         })
-    //     }
-    // })
-    
-// }
-
-// const postLogin = () => {
-//     return
-// }
 
 module.exports = {
     postRegister,
-    // postLogin,
+    postLogin,
     getMessage
 }
