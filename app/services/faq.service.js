1186f83acb12cb2d0bcc9e70b3d8011cd22b72a0
Nicholas Pung <nckpng95@gmail.com>
2019-08-27 20:30:37 -0700

    Added preliminary services for FAQ

--- app/services/faq.service.js
+++ app/services/faq.service.js
@@ -1,13 +1,53 @@
 const mssql = require("../../mssql");
 const TYPES = require("tedious").TYPES;
 
-const getFaq = () => {};
+const getFaq = () => {
+  return mssql.executeProc("Faq_SelectAll");
+};
 
-const postFaq = () => {};
+const postFaq = faq => {
+  return mssql
+    .executeProc("Faq_Insert", sqlRequest => {
+      sqlRequest.addParameter("faqId", TYPES.Int, faq.faqId);
+      sqlRequest.addParameter("question", TYPES.NVarChar, faq.question, {
+        length: 250
+      });
+      sqlRequest.addParameter("answer"),
+        TYPES.NVarChar,
+        faq.answer,
+        {
+          length: 500
+        };
+    })
+    .then(response => {
+      return response.outputParameters;
+    });
+};
 
-const putFaqById = () => {};
+const putFaqById = faq => {
+    return mssql
+    .executeProc("Faq_Update", sqlRequest => {
+      sqlRequest.addParameter("faqId", TYPES.Int, faq.faqId);
+      sqlRequest.addParameter("question", TYPES.NVarChar, faq.question, {
+        length: 250
+      });
+      sqlRequest.addParameter("answer"),
+        TYPES.NVarChar,
+        faq.answer,
+        {
+          length: 500
+        };
+    })
+    .then(response => {
+      return response.outputParameters;
+    });
+};
 
-const deleteFaq = () => {};
+const deleteFaq = id => {
+  return mssql.executeProc("Faq_Delete", sqlRequest => {
+    sqlRequest.addParameter("Id", TYPES.Int, id);
+  });
+};
 
 module.exports = {
   getFaq,
