f68e1fc405855cb784d26d785571ca9e8a92713e
pelmini <vbai@go.pasadena.edu>
2019-08-30 15:11:56 -0700

    Set up Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,32 @@
 import React from "react";
+import axios from "axios";
 
-const Login = () => {
-    return <>Login</>
+const API = "/api/account/login";
+
+class Login extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            data: null
+        }
+    }
+
+    componentDidMount(){
+        axios.post(API)
+            .then(LoginResponse => {
+                console.log(LoginResponse)
+                this.setState({
+                    data: LoginResponse.data
+                })
+            })
+    }
+
+     render() {
+         return(
+              <div>
+              </div>
+         );
+     }
 }
 
 export default Login;
