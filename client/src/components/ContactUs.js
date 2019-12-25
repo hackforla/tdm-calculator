47f338a2b66d19555fcc86adc099b68e99042387
Nicholas Pung <nckpng95@gmail.com>
2019-12-25 11:05:11 -0800

    Example in ContactUs

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useToast } from "../contexts/Toast";
 
 const ContactUs = () => {
-    return <>ContactUs</>
-}
+  const toast = useToast();
+
+  useEffect(() => {
+    toast.add("check it out");
+  }, []);
+  return <>ContactUs</>;
+};
 
 export default ContactUs;
