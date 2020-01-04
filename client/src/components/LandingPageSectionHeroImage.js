8be5779daeb4e3b58f9f6c363c019f52f9c68b7e
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 14:01:21 -0800

    Create LandingPageSectionHeroImage

---
+++ client/src/components/LandingPageSectionHeroImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPageSectionHeroImage = () => {
+    return (
+        <div>
+            <h1>LA TDM</h1>
+            <h2>Better for your project.</h2>
+            <h2>Better for LA.</h2>
+            <button>Start Your Project</button>
+        </div>
+    )
+}
+
+export default LandingPageSectionHeroImage;
