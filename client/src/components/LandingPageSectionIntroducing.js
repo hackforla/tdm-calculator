67b4443bbf92f2b0703526e22ab265e503b31cb3
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 21:41:04 -0800

    Create LandingPageSectionIntroducing

---
+++ client/src/components/LandingPageSectionIntroducing.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LandingPageSectionIntroducing = props => {
+    return (
+        <div>
+            <h2>Introducing</h2>
+            <h1>LA TDM</h1>
+            <p>Lorem ipsum dolor sit <strong>amet</strong>, consectetur adipiscing <strong>elit</strong>, sed do <strong>eiusmod</strong> tempor incididunt ut <strong>labore</strong> et dolore magna aliqua. Tellus id <strong>interdum</strong> velit laoreet id.</p>
+            <img src="" alt="icon" />
+            <img src="" alt="icon" />
+            <img src="" alt="icon" />
+            <img src="" alt="icon" />
+            <img src="" alt="icon" />
+            <img src="" alt="icon" />
+        </div>
+    )
+}
+
+export default LandingPageSectionIntroducing;
