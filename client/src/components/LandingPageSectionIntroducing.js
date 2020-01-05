1690a04d6cb9ca0198297bd4f7bbc83f6a79ee4c
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 22:52:57 -0800

    Rename class to landing-page-center-text

--- client/src/components/LandingPageSectionIntroducing.js
+++ client/src/components/LandingPageSectionIntroducing.js
@@ -3,7 +3,7 @@ import React from "react";
 const LandingPageSectionIntroducing = props => {
     return (
         <div className="landing-page-intro-container">
-            <div className="landing-page-intro-text">
+            <div className="landing-page-center-text">
                 <h2>Introducing</h2>
                 <h1>LA TDM</h1>
                 <p>Lorem ipsum dolor sit <strong>amet</strong>, consectetur adipiscing <strong>elit</strong>, sed do <strong>eiusmod</strong> tempor incididunt ut <strong>labore</strong> et dolore magna aliqua. Tellus id <strong>interdum</strong> velit laoreet id.</p>
