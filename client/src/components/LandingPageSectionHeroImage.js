ca284cf5a73d8042c48a2be80df8600191afdbb6
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 22:08:44 -0800

    Add class and styles to hero image section

--- client/src/components/LandingPageSectionHeroImage.js
+++ client/src/components/LandingPageSectionHeroImage.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const LandingPageSectionHeroImage = () => {
     return (
-        <div>
-            <h1>LA TDM</h1>
-            <h2>Better for your project.</h2>
-            <h2>Better for LA.</h2>
-            <button>Start Your Project</button>
+        <div className="landing-page-hero-image-container">
+            <div className="landing-page-hero-image-text">
+                <h1 style={{ fontWeight: 900, fontSize: 90 }}>LA TDM</h1>
+                <h2 style={{ fontSize: 60 }}>Better for your project.</h2>
+                <h2 style={{ fontSize: 60 }}>Better for LA.</h2>
+                <button className="green-btn">Start Your Project</button>
+            </div>
         </div>
     )
 }
