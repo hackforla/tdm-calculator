3f4cd2fbeee076084e1860a157676c0f7c38fae8
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 22:41:38 -0800

    Add classes to landing page intro div

--- client/src/components/LandingPageSectionIntroducing.js
+++ client/src/components/LandingPageSectionIntroducing.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const LandingPageSectionIntroducing = props => {
     return (
-        <div>
-            <h2>Introducing</h2>
-            <h1>LA TDM</h1>
-            <p>Lorem ipsum dolor sit <strong>amet</strong>, consectetur adipiscing <strong>elit</strong>, sed do <strong>eiusmod</strong> tempor incididunt ut <strong>labore</strong> et dolore magna aliqua. Tellus id <strong>interdum</strong> velit laoreet id.</p>
-            <img src="" alt="icon" />
-            <img src="" alt="icon" />
-            <img src="" alt="icon" />
-            <img src="" alt="icon" />
-            <img src="" alt="icon" />
-            <img src="" alt="icon" />
+        <div className="landing-page-intro-container">
+            <div className="landing-page-intro-text">
+                <h2>Introducing</h2>
+                <h1>LA TDM</h1>
+                <p>Lorem ipsum dolor sit <strong>amet</strong>, consectetur adipiscing <strong>elit</strong>, sed do <strong>eiusmod</strong> tempor incididunt ut <strong>labore</strong> et dolore magna aliqua. Tellus id <strong>interdum</strong> velit laoreet id.</p>
+                <img src="" alt="icon" />
+                <img src="" alt="icon" />
+                <img src="" alt="icon" />
+                <img src="" alt="icon" />
+                <img src="" alt="icon" />
+                <img src="" alt="icon" />
+            </div>
         </div>
     )
 }
