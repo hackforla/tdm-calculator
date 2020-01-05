11c66212ca32dc1d2e2f0b915bb1bdc2c9d0f0ed
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 22:53:16 -0800

    Add class to center text for section end

--- client/src/components/LandingPageSectionEnd.js
+++ client/src/components/LandingPageSectionEnd.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingPageSectionEnd = props => {
     return (
-        <div>
+        <div className="landing-page-center-text">
             <h2>Preparing for a new project?</h2>
             <p>Let us help you get started right.</p>
             <button className="green-btn">Get Started</button>
