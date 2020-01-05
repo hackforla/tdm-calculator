151d630b3c99f0c31bd494bea5d55785791208b3
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 21:50:16 -0800

    Create LandingPageSectionResidentialDev

---
+++ client/src/components/LandingPageSectionResidentialDev.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LandingPageSectionResidentialDev = props => {
+    return (
+        <div>
+            <h4>
+                Residential Real Estate Developer?
+                <Link to="#">How Mobility Plan 2035 affects you  ❯</Link>
+            </h4>
+        </div>
+    )
+}
+
+export default LandingPageSectionResidentialDev;
