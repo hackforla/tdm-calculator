efab76cd6d10cebfda6225737ccfc9f32042b6af
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-04 21:50:28 -0800

    Create LandingPageSectionEnd

---
+++ client/src/components/LandingPageSectionEnd.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPageSectionEnd = props => {
+    return (
+        <div>
+            <h2>Preparing for a new project?</h2>
+            <p>Let us help you get started right.</p>
+            <button className="green-btn">Get Started</button>
+        </div>
+    )
+}
+
+export default LandingPageSectionEnd;
