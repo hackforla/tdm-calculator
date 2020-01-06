8a5823cd6c224cb6033e554491483cf0ff13f85b
Bonnie Wolfe <37763229+ExperimentsInHonesty@users.noreply.github.com>
2020-01-05 17:16:55 -0800

    Revert "Push to /login instead of /admin if login successful"

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = props => {
     password: ""
   };
 
-  const toast = useToast();
+  const toast = useToast()
 
   const loginSchema = Yup.object().shape({
     email: Yup.string()
@@ -33,116 +33,105 @@ const Login = props => {
 
       if (loginResponse.isSuccess) {
         setLoggedInAccount(loginResponse.user);
-        toast.add("Login successful.");
-        history.push("/login");
+        toast.add('Login successful.')
+        history.push("/admin");
       } else if (loginResponse.code === "AUTH_NOT_CONFIRMED") {
         try {
           await accountService.resendConfirmationEmail(email);
           toast.add(`Your email has not been confirmed.
             Please look through your email for a Registration
             Confirmation link and use it to confirm that you
-            own this email address.`);
+            own this email address.`)
           setSubmitting(false);
         } catch (err) {
-          toast.add(
-            `An internal error occurred in sending an email to ${email}. `,
-            err.message
-          );
+          toast.add(`An internal error occurred in sending an email to ${email}. `, err.message)
           setSubmitting(false);
         }
       } else if (loginResponse.code === "AUTH_NO_ACCOUNT") {
         toast.add(`The email ${email} does not correspond to an
           existing account. Please verify the email or register as a
-          new account.`);
+          new account.`)
         setSubmitting(false);
       } else {
         // Presumably loginResponse.code === "AUTH_INVALID_PASSWORD"
         toast.add(`The password is incorrect, please check it
-          and try again or use the Forgot Password feature.`);
+          and try again or use the Forgot Password feature.`)
         setSubmitting(false);
       }
     } catch (err) {
-      toast.add(err.message);
+      toast.add(err.message)
     }
   };
 
   return (
-    <div style={{ flex: "1 0 auto", display: "flex", flexDirection: "column" }}>
-      <div
-        className="tdm-wizard"
-        style={{ flex: "1 0 auto", display: "flex", flexDirection: "row" }}
-      >
-        <div className="tdm-wizard-sidebar"></div>
-        <div
-          className="tdm-wizard-content-container"
-          style={{ justifyContent: "center" }}
-        >
-          <h1 style={{ fontWeight: 500 }}>Welcome to LA's TDM Calculator</h1>
-          <h3 style={{ fontWeight: 100 }}>
-            Please sign into your account to save progress.
-          </h3>
-          <br />
-          <div className="auth-form">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={loginSchema}
-              onSubmit={(values, actions) =>
-                handleSubmit(values, actions, props)
-              }
-            >
-              {({ touched, errors, isSubmitting, values }) => (
-                <Form>
-                  <div className="form-group">
-                    <Field
-                      type="email"
-                      name="email"
-                      value={values.email}
-                      placeholder="Email Address"
-                      className={`form-control ${
-                        touched.email && errors.email ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className="invalid-feedback"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <Field
-                      type="password"
-                      value={values.password}
-                      name="password"
-                      placeholder="Password"
-                      className={`form-control ${
-                        touched.password && errors.password ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="password"
-                      component="div"
-                      className="invalid-feedback"
-                    />
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="btn-submit"
-                    disabled={isSubmitting}
-                  >
-                    {isSubmitting ? "Please wait..." : "Login"}
-                  </button>
-                </Form>
-              )}
-            </Formik>
-          </div>
-          <br />
-          <div>
-            New to the site? <Link to={`/register`}>Register here.</Link>
-          </div>
+    <div style={{ flex: "1 0 auto", display: "flex", flexDirection: "column" }} >
+      <div className="tdm-wizard" style={{ flex: "1 0 auto", display: "flex", flexDirection: "row" }}>
+        <div className="tdm-wizard-sidebar">
         </div>
-      </div>
+        <div className="tdm-wizard-content-container" style={{justifyContent: "center"}}>
+        
+        <h1 style={{fontWeight: 500}}>Welcome to LA's TDM Calculator</h1>
+        <h3 style={{fontWeight: 100}}>Please sign into your account to save progress.</h3>
+        <br />
+        <div className="auth-form">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={loginSchema}
+            onSubmit={(values, actions) => handleSubmit(values, actions, props)}
+          >
+            {({ touched, errors, isSubmitting, values }) => (
+              <Form>
+                <div className="form-group">
+                  <Field
+                    type="email"
+                    name="email"
+                    value={values.email}
+                    placeholder="Email Address"
+                    className={`form-control ${
+                      touched.email && errors.email ? "is-invalid" : ""
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <Field
+                    type="password"
+                    value={values.password}
+                    name="password"
+                    placeholder="Password"
+                    className={`form-control ${
+                      touched.password && errors.password ? "is-invalid" : ""
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="invalid-feedback"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn-submit"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Please wait..." : "Login"}
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <br/>
+        <div>
+          New to the site? <Link to={`/register`}>Register here.</Link>
+        </div>
+      </div>       
     </div>
+  </div>    
   );
 };
 
