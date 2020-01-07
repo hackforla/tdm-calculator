4f2616fd5bca432032755528f5c3ac03bf917648
Mindy Morgan <multimindia@gmail.com>
2020-01-07 15:52:19 -0800

    moving landing page to its own folder

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import LandingPageSectionHeroImage from "./LandingPageSectionHeroImage";
+import LandingPageSectionIntroducing from "./LandingPageSectionIntroducing";
+import LandingPageSectionWhyLATDM from "./LandingPageSectionWhyLATDM";
+import LandingPageSectionMockUp from "./LandingPageSectionMockUp";
+import LandingPageSectionResidentialDev from "./LandingPageSectionResidentialDev";
+import LandingPageSectionEnd from "./LandingPageSectionEnd";
+
+const LandingPage = props => {
+    return (
+        <div>
+            <LandingPageSectionHeroImage />
+            <LandingPageSectionIntroducing />
+            <LandingPageSectionWhyLATDM />
+            <LandingPageSectionMockUp />
+            <LandingPageSectionResidentialDev />
+            <LandingPageSectionEnd />
+        </div>
+    )
+}
+
+export default LandingPage;
