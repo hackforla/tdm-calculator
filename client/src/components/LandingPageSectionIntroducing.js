69b8b5ae93b8009787f39317d8a6084730c53a30
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-06 17:02:45 -0800

    Create and implement classes for intro icons

--- client/src/components/LandingPageSectionIntroducing.js
+++ client/src/components/LandingPageSectionIntroducing.js
@@ -7,12 +7,23 @@ const LandingPageSectionIntroducing = props => {
                 <h2>Introducing</h2>
                 <h1>LA TDM</h1>
                 <p>Lorem ipsum dolor sit <strong>amet</strong>, consectetur adipiscing <strong>elit</strong>, sed do <strong>eiusmod</strong> tempor incididunt ut <strong>labore</strong> et dolore magna aliqua. Tellus id <strong>interdum</strong> velit laoreet id.</p>
-                <img src="" alt="icon" />
-                <img src="" alt="icon" />
-                <img src="" alt="icon" />
-                <img src="" alt="icon" />
-                <img src="" alt="icon" />
-                <img src="" alt="icon" />
+            </div>
+            <div className="intro-icon-container">
+                <div className="intro-icon-item">
+                    <img src="" alt="icon" width="80px" height="80px" />
+                </div>
+                <div className="intro-icon-item">
+                    <img src="" alt="icon" width="80px" height="80px" />
+                </div>
+                <div className="intro-icon-item">
+                    <img src="" alt="icon" width="80px" height="80px" />
+                </div>
+                <div className="intro-icon-item">
+                    <img src="" alt="icon" width="80px" height="80px" />
+                </div>
+                <div className="intro-icon-item">
+                    <img src="" alt="icon" width="80px" height="80px" />
+                </div>
             </div>
         </div>
     )
