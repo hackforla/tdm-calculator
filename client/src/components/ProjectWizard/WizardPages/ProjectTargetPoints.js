a62fecbf9603e8c8a29386dbe1b720071d4ff2f1
Mindy Morgan <multimindia@gmail.com>
2020-02-25 20:54:37 -0800

    #209

--- client/src/components/ProjectWizard/WizardPages/ProjectTargetPoints.js
+++ client/src/components/ProjectWizard/WizardPages/ProjectTargetPoints.js
@@ -1,9 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { createUseStyles } from "react-jss";
 import RuleInputPanels from "../RuleInput/RuleInputPanels";
 
+const useStyles = createUseStyles({
+  page4: {
+    "& > div > div > div": {
+      minWidth: 0,
+      width: 460,
+      margin: "0 auto 10px",
+      left: "-100px",
+      position: "relative",
+      fontSize: 22,
+      fontFamily: "Calibri Bold",
+      "& label": {
+        textAlign: "right",
+        flexBasis: "80%"
+      },
+      "& div:last-child": {
+        flexBasis: "20%"
+      }
+    },
+    "& > div > div > div:first-child": {
+      textAlign: "center",
+      width: "auto",
+      marginBottom: 74,
+      marginTop: 40,
+      left: "auto",
+      display: "block",
+      fontFamily: "Calibri",
+      "& input": {
+        fontSize: 16,
+        height: 45,
+        width: 426
+      }
+    }
+  }
+});
+
 function ProjectTargetPoints(props) {
-  const { rules, onInputChange, classes } = props;
+  const classes = useStyles();
+  const { rules, onInputChange } = props;
   return (
     <div className={classes.page4}>
       <h1 className="tdm-wizard-page-title">Calculate TDM Target Points</h1>
@@ -21,8 +58,7 @@ function ProjectTargetPoints(props) {
 }
 ProjectTargetPoints.propTypes = {
   rules: PropTypes.object.isRequired,
-  onInputChange: PropTypes.func.isRequired,
-  classes: PropTypes.object.isRequired
+  onInputChange: PropTypes.func.isRequired
 };
 
 export default ProjectTargetPoints;
