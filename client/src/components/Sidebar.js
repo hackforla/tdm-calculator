a71a020532f5dd676eed4c503055c2d471aef2b7
Louis Mazza <louismazza@gmail.com>
2020-03-11 13:35:41 -0700

    responsive about page

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { createUseStyles } from "react-jss";
-import { Link } from "react-router-dom";
 
 export const useStyles = createUseStyles({
   "sidebar-container": {
