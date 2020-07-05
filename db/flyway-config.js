abdecc471dd369de3565f525a441d9c2589cdd7f
John Darragh <darragh@entrotech.net>
2020-07-05 11:25:13 -0700

    Local DB Instruction start

--- db/flyway-config.js
+++ db/flyway-config.js
@@ -5,14 +5,21 @@ dotenv.config();
 module.exports = function () {
   const host = process.env.SQL_SERVER_NAME;
   const port = process.env.SQL_SERVER_PORT;
+  const instance = process.env.SQL_SERVER_INSTANCE;
   const databaseName = process.env.SQL_DATABASE_NAME;
   const username = process.env.SQL_USER_NAME;
   const password = process.env.SQL_PASSWORD;
+  // On Windows, SQL Server might have an instance name,
+  // instead of a port.
+  const origin = instance
+    ? `${host}\\${instance}`
+    : port
+    ? `${host}:${port}`
+    : host; // default instance and port
 
   return {
     flywayArgs: {
-      url: `jdbc:sqlserver://${host}:${port};databaseName=${databaseName}`,
-      schemas: "dbo",
+      url: `jdbc:sqlserver://${origin};databaseName=${databaseName}`,
       locations: `filesystem:${__dirname}/migration`,
       user: username,
       password: password,
