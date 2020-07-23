0b7ce97d69489ed8e2a8d9b7ff351d8fff6762e6
John Darragh <darragh@entrotech.net>
2020-07-23 10:53:34 -0700

    upgrade tedious to mssql

--- app/services/project.service.js
+++ app/services/project.service.js
@@ -1,15 +1,13 @@
-const mssql = require("../../mssql");
-const TYPES = require("tedious").TYPES;
+const { pool, poolConnect } = require("./tedious-pool");
+const mssql = require("mssql");
 
 const getAll = async loginId => {
   try {
-    const response = await mssql.executeProc(
-      "Project_SelectAll",
-      sqlRequest => {
-        sqlRequest.addParameter("LoginId", TYPES.Int, loginId);
-      }
-    );
-    return response.resultSets[0];
+    await poolConnect;
+    const request = pool.request();
+    request.input("LoginId", mssql.Int, loginId);
+    const response = await mssql.executeProc("Project_SelectAll");
+    return response.recordset;
   } catch (err) {
     console.log(err);
   }
@@ -17,19 +15,13 @@ const getAll = async loginId => {
 
 const getById = async (loginId, id) => {
   try {
-    const response = await mssql.executeProc(
-      "Project_SelectById",
-      sqlRequest => {
-        sqlRequest.addParameter("LoginId", TYPES.Int, loginId);
-        sqlRequest.addParameter("Id", TYPES.Int, id);
-      }
-    );
-    if (
-      response.resultSets &&
-      response.resultSets[0] &&
-      response.resultSets[0].length > 0
-    ) {
-      return response.resultSets[0][0];
+    await poolConnect;
+    const request = pool.request();
+    request.input("LoginId", mssql.Int, loginId);
+    request.input("Id", mssql.Int, id);
+    const response = await mssql.executeProc("Project_SelectById");
+    if (response.recordset && response.recordset.length > 0) {
+      return response.recordset[0];
     } else {
       return null;
     }
@@ -40,24 +32,17 @@ const getById = async (loginId, id) => {
 
 const post = async item => {
   try {
-    const response = await mssql.executeProc("Project_Insert", sqlRequest => {
-      sqlRequest.addParameter("name", TYPES.NVarChar, item.name, {
-        length: 200
-      });
-      sqlRequest.addParameter("address", TYPES.NVarChar, item.address, {
-        length: 200
-      });
-      sqlRequest.addParameter("description", TYPES.NVarChar, item.description, {
-        length: Infinity
-      });
-      sqlRequest.addParameter("formInputs", TYPES.NVarChar, item.formInputs, {
-        length: Infinity
-      });
-      sqlRequest.addParameter("loginId", TYPES.Int, item.loginId);
-      sqlRequest.addParameter("calculationId", TYPES.Int, item.calculationId);
-      sqlRequest.addOutputParameter("id", TYPES.Int, null);
-    });
-    return response.outputParameters;
+    await poolConnect;
+    const request = pool.request();
+    request.input("name", mssql.NVarChar, item.name); // 200
+    request.input("address", mssql.NVarChar, item.address); // 200
+    request.input("description", mssql.NVarChar, item.description); // max
+    request.input("formInputs", mssql.NVarChar, item.formInputs); // max
+    request.input("loginId", mssql.Int, item.loginId);
+    request.input("calculationId", mssql.Int, item.calculationId);
+    request.output("id", mssql.Int, null);
+    const response = await mssql.executeProc("Project_Insert");
+    return response.output;
   } catch (err) {
     return Promise.reject(err);
   }
@@ -65,23 +50,17 @@ const post = async item => {
 
 const put = async item => {
   try {
-    await mssql.executeProc("Project_Update", sqlRequest => {
-      sqlRequest.addParameter("name", TYPES.NVarChar, item.name, {
-        length: 200
-      });
-      sqlRequest.addParameter("address", TYPES.NVarChar, item.address, {
-        length: 200
-      });
-      sqlRequest.addParameter("description", TYPES.NVarChar, item.description, {
-        length: Infinity
-      });
-      sqlRequest.addParameter("formInputs", TYPES.NVarChar, item.formInputs, {
-        length: Infinity
-      });
-      sqlRequest.addParameter("loginId", TYPES.Int, item.loginId);
-      sqlRequest.addParameter("calculationId", TYPES.Int, item.calculationId);
-      sqlRequest.addParameter("id", TYPES.Int, item.id);
-    });
+    await poolConnect;
+    const request = pool.request();
+
+    request.input("name", mssql.NVarChar, item.name); // 200
+    request.input("address", mssql.NVarChar, item.address); // 200
+    request.input("description", mssql.NVarChar, item.description); // max
+    request.input("formInputs", mssql.NVarChar, item.formInputs); // max
+    request.input("loginId", mssql.Int, item.loginId);
+    request.input("calculationId", mssql.Int, item.calculationId);
+    request.input("id", mssql.Int, item.id);
+    await mssql.executeProc("Project_Update");
   } catch (err) {
     return Promise.reject(err);
   }
@@ -89,10 +68,11 @@ const put = async item => {
 
 const del = async (loginId, id) => {
   try {
-    await mssql.executeProc("Project_Delete", sqlRequest => {
-      sqlRequest.addParameter("loginId", TYPES.Int, loginId);
-      sqlRequest.addParameter("id", TYPES.Int, id);
-    });
+    await poolConnect;
+    const request = pool.request();
+    request.input("loginId", mssql.Int, loginId);
+    request.input("id", mssql.Int, id);
+    await mssql.executeProc("Project_Delete");
   } catch (err) {
     return Promise.reject(err);
   }
