99392cb2ef3cbc01f0958327ac0429cfdcce3266
James Higgs <james.y.higgs@gmail.com>
2020-08-06 17:31:39 -0700

    Added Tooltips to Sidebar

--- client/src/components/ProjectWizard/SidebarPoints/ToolTip.js
+++ client/src/components/ProjectWizard/SidebarPoints/ToolTip.js
@@ -20,46 +20,58 @@ const useStyles = createUseStyles({
     }
   },
   tipText: {
-    display: "inline-block",
-    backgroundColor: "#A7C539",
-    color: "white",
-    width: "160px",
-    fontFamily: "Calibri",
-    fontSize: "16px",
+    display: "none",
+    padding: "15px",
+    backgroundColor: "rgb(230, 234, 239)",
+    width: "200px",
+    fontFamily: "Arial",
     textAlign: "center",
-    padding: "5px 0",
-    borderRadius: 6,
-    position: "absolute",
-    "z-index": 1,
+    fontSize: 12,
+    lineHeight: "16px",
     marginLeft: 20,
+    fontWeight: "bold",
+    "-webkit-box-shadow": "0px 0px 8px rgba(0, 46, 109, 0.2)",
+    "-moz-box-shadow": "0px 0px 8px rgba(0, 46, 109, 0.2)",
+    boxShadow: "0px 0px 8px rgba(0, 46, 109, 0.2)",
+    "-webkit-border-radius": 2,
+    "-moz-border-radius": 2,
+    borderRadius: 2,
     visibility: "hidden",
     opacity: 0,
     transition: "opacity 0.3s",
-    "&.showTip": {
-      visibility: "visible",
-      opacity: 1
+    "z-index": 1,
+    "&.show": {
+      display: "inline-block",
+      position: "absolute !important",
+      visibility: "visible !important",
+      color: "rgb(30, 36, 63) !important",
+      backgroundColor: "rgb(230, 234, 239) !important",
+      opacity: "1 !important",
+      textTransform: "capitalize !important"
     }
   }
 });
 
 const ToolTip = ({ tipText }) => {
-  const [tipVisibility] = useState(false);
+  const [tipVisibility, setTipVisibility] = useState(false);
   const classes = useStyles();
   /*eslint-disable */
-  const handleClick = () => {
-    //setTipVisibility(!tipVisibility);
+  const handleHover = () => {
+    setTipVisibility(!tipVisibility);
   };
   /*eslint-enable */
-  const showTip = tipVisibility ? "showTip" : "";
+  const showTip = tipVisibility ? "show" : "";
   return (
     <React.Fragment>
-      <ToolTipIcon
-        containerStyle={{
-          fontSize: 16,
-          verticalAlign: "top",
-          "&:hover": { cursor: "pointer" }
-        }}
-      />
+      <span onMouseEnter={handleHover} onMouseLeave={handleHover}>
+        <ToolTipIcon
+          containerStyle={{
+            fontSize: 16,
+            verticalAlign: "top",
+            "&:hover": { cursor: "pointer" }
+          }}
+        />
+      </span>
       <span className={clsx(classes.tipText, showTip)}>{tipText}</span>
     </React.Fragment>
   );
