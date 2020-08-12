9aa2c5bb484fd4fec815947bb588c7f001861fd8
croor005 <richard.crook@disney.com>
2020-08-12 10:01:39 -0700

    Adding verifiction for this workflow

--- cypress/integration/project-calculations-no-auths/barrington-condos.spec.js
+++ cypress/integration/project-calculations-no-auths/barrington-condos.spec.js
@@ -3,18 +3,16 @@ import "@testing-library/cypress/add-commands";
 
 /// <reference types="cypress" />
 describe("Barrington Condos", () => {
-
   context("project inputs", () => {
     it("enters project information - minimum requirements", () => {
-
-      cy.loginViaUIAs("test_regular_user@dispostable.com","Dogfood1")
+      cy.loginViaUIAs("test_regular_user@dispostable.com", "Dogfood1");
 
       cy.findByLabelText("Project Name").type("Barrington Condos");
       cy.findByLabelText("Address").type("825 S. Barrington Ave");
       cy.get("[data-testid=APN]").type("1234567890");
-      cy.get('#APN').then(($element => {
-           cy.wrap($element.val()).as('apn_number')
-      }));
+      cy.get("#APN").then($element => {
+        cy.wrap($element.val()).as("apn_number");
+      });
       cy.get(".space-between").click();
       cy.get("#UNITS_CONDO").type("46");
       cy.get("#PARK_CONDO").type("92");
@@ -32,55 +30,79 @@ describe("Barrington Condos", () => {
         "35% of State Density Bonus"
       );
       cy.get(".space-between > :nth-child(2)").click();
-      cy.get(".tdm-wizard-content-container.contentContainer-0-2-26 > div > div:nth-child(3) > span:nth-child(1)").should("have.text","Barrington Condos")
-      cy.get(".tdm-wizard-content-container.contentContainer-0-2-26 > div > div:nth-child(3) > span:nth-child(2)").should("contain.text","825 S. Barrington Ave")
-      cy.get(".tdm-wizard-content-container > div > div > div > div:nth-child(2) > span:nth-child(2)").then(($projectDetailsAPN) => {
-        cy.get('@apn_number').then(($projectWizardAPN => {
-          expect($projectDetailsAPN.text()).to.equal($projectWizardAPN)
-        }))
+      cy.get(
+        ".tdm-wizard-content-container.contentContainer-0-2-26 > div > div:nth-child(3) > span:nth-child(1)"
+      ).should("have.text", "Barrington Condos");
+      cy.get(
+        ".tdm-wizard-content-container.contentContainer-0-2-26 > div > div:nth-child(3) > span:nth-child(2)"
+      ).should("contain.text", "825 S. Barrington Ave");
+      cy.get(
+        ".tdm-wizard-content-container > div > div > div > div:nth-child(2) > span:nth-child(2)"
+      ).then($projectDetailsAPN => {
+        cy.get("@apn_number").then($projectWizardAPN => {
+          expect($projectDetailsAPN.text()).to.equal($projectWizardAPN);
+        });
       });
 
-      cy.get("[data-testid='summary-project-level-value']").should("have.text", "1")
-      cy.get("[data-testid='summary-parking-ratio-value']").then(($ele => {
-        expect($ele.text()).to.contain("95")
-      }))
+      cy.get("[data-testid='summary-project-level-value']").should(
+        "have.text",
+        "1"
+      );
+      cy.get("[data-testid='summary-parking-ratio-value']").then($ele => {
+        expect($ele.text()).to.contain("95");
+      });
 
-      cy.get("[data-testid='summary-target-points-value']").then(($ele => {
-        expect($ele.text()).to.contain("15")
-      }))
+      cy.get("[data-testid='summary-target-points-value']").then($ele => {
+        expect($ele.text()).to.contain("15");
+      });
 
-      cy.get("[data-testid='summary-earned-points-value']").then(($ele => {
-        expect($ele.text()).to.contain("4")
-      }))
-     cy.get(".tdm-wizard-content-container > div > div:nth-child(5) > div > span").should("contain.text", "Residential")
-    //   cy.findAllByText("Residential").should("exist");
-    //
-    //   cy.findByTestId("summary-project-level-value").should("have.text", "1");
-    //   cy.findByTestId("summary-project-level-label").should(
-    //     "have.text",
-    //     "Project Level"
-    //   );
-    //
-    //   cy.findByTestId("summary-parking-ratio-value").should(
-    //     "have.text",
-    //     "95 %"
-    //   );
-    //   cy.findByTestId("summary-parking-ratio-label").should(
-    //     "have.text",
-    //     "Parking Provided / Baseline"
-    //   );
-    //
-    //   cy.findByTestId("summary-target-points-value").should("have.text", "15");
-    //   cy.findByTestId("summary-target-points-label").should(
-    //     "have.text",
-    //     "Target Points"
-    //   );
-    //
-    //   cy.findByTestId("summary-earned-points-value").should("have.text", "16");
-    //   cy.findByTestId("summary-earned-points-label").should(
-    //     "have.text",
-    //     "Earned Points"
-    //   );
-     });
+      cy.get("[data-testid='summary-earned-points-value']").then($ele => {
+        expect($ele.text()).to.contain("4");
+      });
+      cy.get(
+        ".tdm-wizard-content-container > div > div:nth-child(5) > div > span"
+      ).should("contain.text", "Residential");
+
+      cy.get(
+        ":nth-child(1) > .justify-content-center > .ruleText-0-2-207"
+      ).should("have.text", "35% of State Density Bonus");
+      cy.get(
+        ".tdm-wizard-content-container > div > div:nth-child(6) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1)"
+      ).should("have.text", "2");
+
+      cy.get(
+        ":nth-child(2) > .justify-content-center > .ruleText-0-2-207"
+      ).should("have.text", "the cost of each parking space is $25/mo");
+      cy.get(
+        ".tdm-wizard-content-container > div > div:nth-child(6) > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(1)"
+      ).should("have.text", "1");
+
+      cy.get(
+        ":nth-child(3) > .justify-content-center > .ruleText-0-2-207"
+      ).should(
+        "have.text",
+        "Reduces 25% of the parking spaces available relative to the  parking baseline"
+      );
+      cy.get(
+        ".tdm-wizard-content-container > div > div:nth-child(6) > div > div:nth-child(3) > div:nth-child(3) > div:nth-child(1)"
+      ).should("have.text", "1");
+
+      cy.get(
+        ":nth-child(1) > .specificationDetailsContainer-0-2-205 > .measureDetails-0-2-202"
+      ).should("have.text", "46");
+      cy.get(
+        ":nth-child(2) > .specificationDetailsContainer-0-2-205 > .measureDetails-0-2-202"
+      ).should("have.text", "92");
+      cy.get(
+        ":nth-child(2) > .measuresContainer-0-2-200 > .rule-0-2-175 > .pointsContainer-0-2-206 > .value-0-2-178"
+      ).should("have.text", "92");
+      cy.get(
+        ":nth-child(4) > .pointsContainer-0-2-206 > .value-0-2-178"
+      ).should("have.text", "88");
+
+      cy.get(".projectLevelValue-0-2-43").should("have.text", "1");
+      cy.get(":nth-child(1) > .ruleValue-0-2-54").should("have.text", "15");
+      cy.get(":nth-child(2) > .ruleValue-0-2-54").should("have.text", "4");
+    });
   });
 });
