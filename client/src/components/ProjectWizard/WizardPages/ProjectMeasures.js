19ca11138bb2f30a7191d3c106d397415a5ca189
John Darragh <darragh@entrotech.net>
2020-08-15 09:31:19 -0700

    Separate Medical Inputs for Hospitals and Convalescent

--- client/src/components/ProjectWizard/WizardPages/ProjectMeasures.js
+++ client/src/components/ProjectWizard/WizardPages/ProjectMeasures.js
@@ -128,7 +128,7 @@ function ProjectMeasure(props) {
         </button>
       </div>
       <RuleStrategyPanels
-        rules={rules.filter(rules => rules.display)}
+        rules={rules}
         onInputChange={onInputChange}
         onCommentChange={onCommentChange}
       />
