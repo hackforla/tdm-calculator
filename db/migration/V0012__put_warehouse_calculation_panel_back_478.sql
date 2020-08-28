163dd9dae269c5ff1e01a65013136b8b13464ad0
John Darragh <darragh@entrotech.net>
2020-08-27 17:25:00 -0700

    Add main branch to github action

--- db/migration/V0012__put_warehouse_calculation_panel_back_478.sql
+++ db/migration/V0012__put_warehouse_calculation_panel_back_478.sql
@@ -1,16 +1,16 @@
 SET IDENTITY_INSERT CalculationPanel ON;
 
-INSERT INTO dbo.CalculationPanel
-           (id, calculationId
-           ,name
-           ,cssClass
-           ,displayOrder)
-     VALUES
-           (33, 1
-           ,'Warehouse Space'
-           ,'landUse'
-           ,350
-           )
-GO
+-- INSERT INTO dbo.CalculationPanel
+--            (id, calculationId
+--            ,name
+--            ,cssClass
+--            ,displayOrder)
+--      VALUES
+--            (33, 1
+--            ,'Warehouse Space'
+--            ,'landUse'
+--            ,350
+--            )
+-- GO
 
 SET IDENTITY_INSERT CalculationPanel OFF;
