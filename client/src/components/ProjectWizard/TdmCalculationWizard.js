b7832bc0d4751fd3481db6192ff049cc1fbd3826
James Higgs <james.y.higgs@gmail.com>
2020-10-11 20:45:35 -0700

    Finished implementing Terms and Conditions Modal

--- client/src/components/ProjectWizard/TdmCalculationWizard.js
+++ client/src/components/ProjectWizard/TdmCalculationWizard.js
@@ -553,40 +553,41 @@ const TdmCalculationWizard = props => {
 
           <p className="smaller">
             The Los Angeles Department of Transportation (LADOT), in partnership
-            with the Department of City Planning and Hack for LA, has developed
-            the City of Los Angeles Transportation Demand Management (TDM)
-            Calculator (TDM Calculator) to provide the public with an
-            understanding of the TDM Ordinance that is currently available for
-            review: link to (CF XX-XXXX / CPC Case). The TDM Ordinance proposes
-            to revise regulations that require eligible land use development
-            projects to adopt TDM strategies with the goal to reduce the
-            reliance on drive-alone trips in the City of Los Angeles. The term
-            “City” as used below shall refer to the City of Los Angeles. The
-            terms “City” and “Hack for LA” as used below shall include their
-            respective affiliates, subconsultants, employees, volunteers, and
+            with the Department of City Planning and Hack for LA (a project of
+            Code for America), has developed the City of Los Angeles
+            Transportation Demand Management (TDM) Calculator (TDM Calculator)
+            to provide the public with an understanding of the TDM Ordinance.
+            Currently available for review: Council File 15-0719-S19 / the Los
+            Angeles Department of City Planning website). The TDM Ordinance
+            proposes to revise regulations that require eligible land use
+            development projects to adopt TDM strategies with the goal to reduce
+            the reliance on drive-alone trips in the City of Los Angeles. The
+            term “City” as used below shall refer to the City of Los Angeles.
+            The terms “City” and “Hack for LA” as used below shall include their
+            respective affiliates, sub-consultants, employees, volunteers, and
             representatives.
           </p>
 
           <p className="smaller">
-            This application, the TDM Calculator, has been provided to You, the
-            User, as a public service to assess different scenarios that
-            development would need to comply with if the City were to adopt the
-            TDM Ordinance. LADOT is pleased to be able to provide this
-            information to the public. LADOT believes that the public is most
-            effectively served when they are provided access to the technical
-            tools that inform the public policy-making process that govern
-            private and public land use investments. However, in using the TDM
-            Calculator, You agree to be bound by this TDM Calculator User
-            Agreement (this Agreement).
+            This digital review tool, the TDM Calculator, has been provided to
+            You, the User, as a public service to assess different scenarios
+            that land use development would need to comply with if the City were
+            to adopt the TDM Ordinance. LADOT is pleased to be able to provide
+            this information to the public. LADOT believes that the public is
+            most effectively served when they are provided access to the
+            technical tools that inform the public policy-making process that
+            governs private and public land-use investments. However, in using
+            the TDM Calculator, You agree to be bound by this TDM Calculator
+            User Agreement (this Agreement).
           </p>
 
           <p className="smaller">
             <strong>Limited License to Use.</strong> This Agreement gives You a
-            limited, non-transferrable, non-assignable, and non-exclusive
-            license to use the TDM Calculator on a computer system owned, leased
-            or otherwise controlled by You in Your own facilities, as set out
-            below, and provided that You know and follow the terms of this
-            Agreement. Your failure to follow the terms of this Agreement shall
+            limited, non-transferable, non-assignable, and non-exclusive license
+            to use the TDM Calculator on a computer system owned, leased, or
+            otherwise controlled by You in Your own facilities, as set out below
+            and provided that You know and follow the terms of this Agreement.
+            Your failure to follow the terms of this Agreement shall
             automatically terminate this license and Your right to use the TDM
             Calculator.
           </p>
@@ -602,37 +603,45 @@ const TdmCalculationWizard = props => {
             Ordinance is a draft regulation and not current law, it could
             further change, or be adopted in whole, or in part, denied, or
             abandoned, the information herein should not be interpreted to be
-            binding on development regulation outcomes that inform investment
-            decisions. Due to the dynamic nature of the information contained
-            within the TDM Calculator and the reliance upon information from
-            outside sources, the City does not guarantee the accuracy or
-            reliability of the information transmitted from this web site. The
-            TDM Calculator, OUTPUTS AND ASSOCIATED DATA ARE PROVIDED “as is”
-            WITHOUT WARRANTY OF ANY KIND, whether expressed, implied, statutory,
-            or otherwise including but not limited to, warranties of title or
-            the implied warranties of merchantability and fitness for a
-            particular purpose. The City nor Hack for LA are not responsible for
-            any special, indirect, incidental, or consequential damages that may
-            arise from the use of, or the inability to use, the data and/or the
-            materials contained on the data whether the materials contained on
-            the data are provided by the City, or a third party.
+            binding on land use development regulation outcomes that inform
+            investment decisions. Due to the dynamic nature of the information
+            contained within the TDM Calculator and the reliance upon
+            information from outside sources, the City does not guarantee the
+            accuracy or reliability of the information transmitted from this web
+            site. The TDM Calculator, OUTPUTS AND ASSOCIATED DATA ARE PROVIDED
+            “as is” WITHOUT WARRANTY OF ANY KIND, whether expressed, implied,
+            statutory or otherwise including but not limited to, warranties of
+            title or the implied warranties of merchantability and fitness for a
+            particular purpose. Neither the City nor Hack for LA are responsible
+            for any special, indirect, incidental, or consequential damages that
+            may arise from the use of, or the inability to use, the data and/or
+            the materials contained on the data whether the materials contained
+            on the data are provided by the City or a third party.
+          </p>
+
+          <p className="smaller">
+            LADOT is neither responsible nor liable for any viruses or other
+            contamination of your system nor for any delays, inaccuracies,
+            errors or omissions arising out of your use of the TDM Calculator or
+            with respect to the material contained within the TDM Calculator.
           </p>
 
           <p className="smaller">
             <strong>Limitation of Liability.</strong> It is understood that the
             TDM Calculator is provided without charge. Neither the City nor Hack
             for LA can be responsible or liable for any information derived from
-            its use, or for any delays, inaccuracies, incompleteness, errors or
+            its use, nor liable for any viruses or other contamination of your
+            system nor for any delays, inaccuracies, incompleteness, errors, or
             omissions arising out of your use of the TDM Calculator or with
             respect to the material contained in the TDM Calculator. You
             understand and agree that Your sole remedy against the City or Hack
             for LA for loss or damage caused by any defect or failure of the TDM
             Calculator, regardless of the form of action, whether in contract,
-            tort, including negligence, strict liability or otherwise, shall be
+            tort, including negligence, strict liability, or otherwise, shall be
             the repair or replacement of the TDM Calculator to the extent
             feasible as determined solely by the City. In no event shall the
             City or Hack for LA be responsible to You or anyone else for, or
-            have liability for any special, indirect, incidental or
+            have liability for any special, indirect, incidental, or
             consequential damages (including, without limitation, damages for
             loss of business profits or changes to businesses costs) or lost
             data or downtime, however caused, and on any theory of liability
@@ -646,11 +655,11 @@ const TdmCalculationWizard = props => {
           <p className="smaller">
             This Agreement and License shall be governed by the laws of the
             State of California without regard to their conflicts of law
-            provisions, and shall be effective as of the date set forth below
-            and, unless terminated in accordance with the above or extended by
-            written amendment to this Agreement, shall terminate on the earlier
-            of the date that You are not making use of the TDM Calculator or one
-            year after the beginning of Your use of the TDM Calculator.
+            provisions and shall be effective as of the date that You are making
+            use of the TDM Calculator. Any action brought to enforce the
+            Agreement and/or in connection with this website shall be brought in
+            either the state or federal courts in the County of Los Angeles
+            County, State of California.
           </p>
 
           <p className="smaller">
@@ -662,8 +671,11 @@ const TdmCalculationWizard = props => {
 
           <p className="smaller">
             Before making decisions using the information provided in this
-            application, contact City LADOT staff at ladot.tdm@lacity.org to
-            confirm the validity of the data provided.
+            application, contact City LADOT staff at{" "}
+            <a href="mailto:ladot.tdm@lacity.org" style={{ color: "blue" }}>
+              ladot.tdm@lacity.org
+            </a>{" "}
+            to confirm the validity of the data provided.
           </p>
         </div>
 
