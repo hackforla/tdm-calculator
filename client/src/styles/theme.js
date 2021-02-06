40f72f23a6ff0f85ce516e05ff9448a0f73a3d79
James Higgs <james.y.higgs@gmail.com>
2021-02-06 10:33:49 -0800

    Refactored ticket

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -5,6 +5,7 @@ module.exports = {
     colorPrimary: "#a7c539", //lime green
     colorText: "#0F2940", //dark blue
     colorLADOT: "#002E6D",
+    colorEarnedPoints: "rgb(255, 168, 4)", //orange
     colorDisabled: "rgba(0, 0, 0, .05)", //lightest grey transparent
     colorCancel: "rgba(0, 0, 0, 0.5)", //light grey, e.g. cancel button
     colorWhite: "#fff", //white
