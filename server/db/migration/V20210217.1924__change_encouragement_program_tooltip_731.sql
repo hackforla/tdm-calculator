13ceac6596536eb997bb3ef507f8fa4accd7e572
Jonathan Ko <jonathan.ko523@gmail.com>
2021-02-17 19:42:52 -0800

    #731 Change Encouragement Tooltip

---
+++ server/db/migration/V20210217.1924__change_encouragement_program_tooltip_731.sql
@@ -0,0 +1,20 @@
+UPDATE CalculationRule SET 
+description='<ol style="margin-left:1em">
+	<li> Education, Marketing and Outreach: Offer new employees and residents a 
+	packet of materials and/or provide personal consultation detailing sustainable (non-SOV) 
+	travel options. These materials or consultation must be available on an ongoing 
+	basis and/or on permanent online channels. Packet must include the 
+	distribution of a one Metro TAP card pre-loaded with a trip, to each 
+	employee or residential unit.</li>
+	<li style="margin-top:0.5em"> Travel Behavior Change Program: A multi-faceted program typically 
+	involving two-way communication campaigns and travel feedback that actively 
+	engages participants to target individual attitudes, goals, and travel behaviors 
+	to alter their travel choices and habits. Program must include the distribution 
+	of a one Metro TAP card pre-loaded with a trip, to each employee or residential unit. 
+	Selection of this strategy requires a coordinator to manage the program. </li>
+	<li style="margin-top:0.5em"> 
+		NOTE: Cannot be combined with Required Trip-Reduction Program strategy.
+	</li>
+</ol>'
+
+WHERE calculationId=1 AND code='STRATEGY_INFO_3';
