f9e0c33f6c28512729bf19d115f06d5c6508b19c
Jonathan Ko <jonathan.ko523@gmail.com>
2021-03-17 18:54:02 -0700

    Updated About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -17,6 +17,11 @@ const useStyles = createUseStyles({
   link: {
     textDecoration: "underline",
     fontWeight: "bold"
+  },
+  linklist: {
+    textDecoration: "underline",
+    fontWeight: "bold",
+    marginBottom: "0.8em"
   }
 });
 
@@ -25,39 +30,89 @@ const About = () => {
 
   return (
     <ContentContainer componentToTrack="AboutPage">
-      <h1>About TDM Calculator</h1>
+      <h1>About the TDM Calculator</h1>
       <br />
       <div className={classes.aboutText}>
+        <h3 className="tdm-wizard-page-subtitle">What does TDM mean?</h3>
         <p>
-          The Transportation Demand Management Calculator was created by Hack
-          For L.A in conjunction with L.A. Department of Transportation and
-          Department of Planning.
+          Transportation Demand Management (TDM) refers to the strategies a city
+          uses to influence travel behavior and accommodate growing demands on
+          its transportation system.
+        </p>
+        <h3 className="tdm-wizard-page-subtitle">TDM Program Update</h3>
+        <p>
+          Los Angeles Department of Transportation (LADOT) and Los Angeles City
+          Planning (LACP) are working together to update the City’s TDM
+          ordinance to provide Angelenos with a variety of sustainable
+          transportation options, increase accessibility throughout the City,
+          and reduce single-occupancy vehicle trips, which benefits the
+          environment, public health, and can ease congestion.
+        </p>
+        <h3 className="tdm-wizard-page-subtitle">
+          What is the TDM Calculator?
+        </h3>
+        <p>
+          The TDM Calculator is a tool developed to help simplify the
+          development and implementation of the proposed updates to the City’s
+          TDM regulations. The TDM Calculator helps communicate the compliance
+          requirements of the proposed TDM Program to multiple stakeholders
+          including developers of real estate projects, residents, and
+          transportation planning practitioners. This project will ultimately
+          help Los Angeles meet the sustainable transportation goals of the
+          City’s Mobility Plan 2035 and the Mayor’s Green New Deal.
         </p>
         <p>
-          Hack for LA is a non-profit organization making an impact in Los
-          Angeles through building digital products, programs and services using
-          agile methodology and user centered design.
+          The TDM Calculator was created by Hack For LA in collaboration with
+          the Los Angeles Department of Transportation (LADOT) and the Los
+          Angeles Department of City Planning (LACP). Hack for LA is a local
+          non-profit organization making an impact in Los Angeles through
+          building digital products, programs and services using Lean-Agile
+          methodology and user centered design.
         </p>
         <p>
-          Working with the LADOT, Hack for LA’s business analysts, software
-          developers and designers, built the TDM Calculator through short
-          iterative sprints.
-        </p>
-        <p>
-          This tool was developed to help simplify the development and
-          implementation of new TDM regulations for multiple stakeholders
-          including the LADOT, Department of Planning, and developers of large
-          real estate projects. This project will ultimately help Los Angeles
-          meet it’s Mobility Plan goals by 2035.
-        </p>
-        <p>
-          To find more about Hack for LA and their work to improve Los Angeles
-          please visit:{" "}
+          Learn more about{" "}
           <a className={classes.link} href="http://www.hackforla.org">
-            www.hackforla.org
+            Hack for LA
           </a>
         </p>
+        <h3 className="tdm-wizard-page-subtitle">
+          Status of the TDM Calculator
+        </h3>
+        <p>
+          The TDM Calculator is in a beta phase and is still undergoing testing
+          and development. A public use version of the TDM Calculator will be
+          developed after the updated TDM ordinance is approved by the City
+          Council and Mayor.{" "}
+        </p>
+        <h3 className="tdm-wizard-page-subtitle">Additional Resources</h3>
+        <ul>
+          <li className={classes.linklist}>
+            <a
+              className={classes.link}
+              href="https://planning.lacity.org/plans-policies/initiatives-policies/mobility"
+            >
+              Los Angeles City Planning, Mobility
+            </a>
+            , Proposed Ordinances and Initiatives{" "}
+          </li>
+          <li className={classes.linklist}>
+            <a href="https://www.youtube.com/watch?v=mAxseCqySuM">
+              Informational Video (January 2021)
+            </a>
+          </li>
+          <li className={classes.linklist}>
+            <a href="https://planning.lacity.org/odocument/d7e3780b-3155-44a4-98cf-0fd673a6612b/TDM-FactSheet_English.pdf">
+              Fact Sheet - English (January 2021)
+            </a>
+          </li>
+          <li className={classes.linklist}>
+            <a href="https://planning.lacity.org/odocument/9dae2614-5b29-4dce-8b8d-9060f6900386/TDM-FactSheet_Spanish.pdf">
+              Hoja Informativa - Español (Enero 2021)
+            </a>
+          </li>
+        </ul>
       </div>
+
       <p>{`Release #: ${version}`}</p>
     </ContentContainer>
   );
