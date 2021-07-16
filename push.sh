968c7b42c88c50e77322d4be35bcb1d504b1ce12
nclairesays <45305029+nclairesays@users.noreply.github.com>
2021-07-15 22:23:09 -0700

    Add conditional to push script to abort if on develop branch #806

--- push.sh
+++ push.sh
@@ -3,12 +3,15 @@ set -u
 
 git fetch || exit 1
 
-# Make sure you are on a branch
-# Condition to check if i'm not on origin/develop
-
-
+# Abort script is on develop branch
+CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$CURRENT_BRANCH" = "develop" ]]; then
+  echo 'Aborting script. You are currently on the develop branch. Please make a feature branch with your commit(s) to run script. ';
+  exit 1;
+fi
 
 # Run frontend tests if the client directory in the current branch is different from origin/develop
+# and automatically fix linter problems, if possible
 client_file_count=$(git diff --name-only origin/develop HEAD | grep -c ^client)
 if [ "$client_file_count" -gt 0 ]
 then
@@ -32,7 +35,6 @@ else
   echo 'Skipping backend server tests because no relevant changes were found'
 fi
 
-
 # Run cypress tests if any file in the cypress, server, or client directories
 # in the current branch are different from origin/develop
 cypress_file_count=$(git diff --name-only origin/main HEAD | grep -c ^cypress)
@@ -46,4 +48,5 @@ else
   echo 'Skipping cypress journey tests because no relevant changes were found'
 fi
 
-# git push origin HEAD
+# Push current local branch to a remote branch on github
+git push origin HEAD
