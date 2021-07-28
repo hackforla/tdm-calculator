8dc93e4552d10950f7a67bcad6c01ce42a211e15
Fang Liu <fangyiliu@gmail.com>
2021-07-28 12:05:01 -0700

    #881 remove Sq Ft from page 2

---
+++ server/db/migration/V20210722.1337__update_sqft_number_of_alternative_881_876_869.sql
@@ -0,0 +1,54 @@
+/**
+ * #881 Remove "Sq Ft - " from page 2
+ */
+UPDATE CalculationRule
+SET name='Retail'
+WHERE calculationId=1 and code='SF_RETAIL';
+
+UPDATE CalculationRule
+SET name='Restaurant/Bar/General'
+WHERE calculationId=1 and code='SF_RESTAURANT';
+
+UPDATE CalculationRule
+SET name='Health Club'
+WHERE calculationId=1 and code='SF_HEALTH_CLUB';
+
+UPDATE CalculationRule
+SET name='Office, Business, Manufacturing, Industrial'
+WHERE calculationId=1 and code='SF_OFFICE';
+
+UPDATE CalculationRule
+SET name='Retail Furniture'
+WHERE calculationId=1 and code='SF_FURNITURE';
+
+UPDATE CalculationRule
+SET name='Warehouse'
+WHERE calculationId=1 and code='SF_WAREHOUSE';
+
+UPDATE CalculationRule
+SET name='Medical Offices, Clinics, Service Facilities'
+WHERE calculationId=1 and code='SF_INST_MEDICAL_SVC';
+
+UPDATE CalculationRule
+SET name='Other Institutional'
+WHERE calculationId=1 and code='SF_INST_OTHER';
+
+UPDATE CalculationRule
+SET name='Government Institution'
+WHERE calculationId=1 and code='SF_INST_GOV';
+
+UPDATE CalculationRule
+SET name='..... Trade School'
+WHERE calculationId=1 and code='SF_TRADE_SCHOOL';
+
+UPDATE CalculationRule
+SET name='Take-out Restaurant'
+WHERE calculationId=1 and code='SF_RESTAURANT_TAKEOUT';
+
+UPDATE CalculationRule
+SET name='Hospital'
+WHERE calculationId=1 and code='SF_HOSPITAL';
+
+UPDATE CalculationRule
+SET name='Convalescent'
+WHERE calculationId=1 and code='SF_CONVALESCENT';
