85a618397ec878b6a8813e05d0e3a98ebb377fdb
nclairesays <45305029+nclairesays@users.noreply.github.com>
2021-08-01 19:10:10 -0700

    Fix script in cypress/package.json

--- push.sh
+++ push.sh
@@ -10,7 +10,7 @@ if [[ "$CURRENT_BRANCH" = "develop" ]]; then
   exit 1;
 fi
 
-# Run frontend tests if the client directory in the current branch is different from origin/develop
+# Run frontend tests if the client directory has changes on the current branch and is different from origin/develop
 # and automatically fix linter problems, if possible
 client_file_count=$(git diff --name-only origin/develop HEAD | grep -c ^client)
 if [ "$client_file_count" -gt 0 ]
@@ -23,7 +23,7 @@ else
   echo 'Skipping frontend tests because no relevant changes were found'
 fi
 
-# Run backend tests if the server directory in the current branch is different from origin/develop
+# Run backend tests if the server directory has changes on the current branch and is different from origin/develop
 server_file_count=$(git diff --name-only origin/develop HEAD | grep -c ^server)
 if [ "$server_file_count" -gt 0 ]
 then
@@ -35,8 +35,8 @@ else
   echo 'Skipping backend server tests because no relevant changes were found'
 fi
 
-# Run cypress tests if any file in the cypress, server, or client directories
-# in the current branch are different from origin/develop
+# Run cypress tests if any file in the cypress, server, or client directories on the
+# current branch are different from origin/develop
 cypress_file_count=$(git diff --name-only origin/main HEAD | grep -c ^cypress)
 if [ "$server_file_count" -gt 0 ] || [ "$client_file_count" -gt 0 ] || [ "$cypress_file_count" -gt 0 ]
 then
