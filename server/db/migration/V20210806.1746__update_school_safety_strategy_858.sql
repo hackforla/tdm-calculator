879a3c4ea1ec898b1530a2be4da7bc0a65aa0ce8
James Higgs <james.y.higgs@gmail.com>
2021-08-11 18:36:47 -0700

    overwrite previous commit regarding school safety strategy

--- server/db/migration/V20210806.1746__update_school_safety_strategy_858.sql
+++ server/db/migration/V20210806.1746__update_school_safety_strategy_858.sql
@@ -1,9 +1,3 @@
 update CalculationRule set
-displayFunctionBody = 'return !!<<LAND_USE_HOTEL>> || 
-	!!<<LAND_USE_WAREHOUSE>> ||
-	!!<<LAND_USE_MEDICAL>> || 
-	!!<<LAND_USE_OTHER>> ||
-	!!<<LAND_USE_SCHOOL>> ||
-	!!<<LAND_USE_MAJOR>>;
-	'
+displayFunctionBody = 'return !!<<LAND_USE_SCHOOL>>;'
 where calculationId = 1 and code = 'STRATEGY_INFO_5'
