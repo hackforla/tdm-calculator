711c3dc5698d91fcaf4d435d29533bbc11267627
Fang Liu <fangyiliu@gmail.com>
2021-08-17 23:16:53 -0700

    Set project on each test

--- cypress/cypress/integration/save-button.spec.js
+++ cypress/cypress/integration/save-button.spec.js
@@ -3,28 +3,61 @@ import '@testing-library/cypress/add-commands';
 /// <reference types="cypress"/>
 
 describe('Save Button', () => {
+  const postNewProject = () => {
+    return cy.request({
+      method: 'POST',
+      url: 'http://localhost:5000/api/projects',
+      body: {
+        name: 'Some Project Hotel',
+        address: '12425 Hotel Bl.',
+        formInputs:
+          '{"PROJECT_NAME":"Some Project Hotel",\
+          "PROJECT_ADDRESS":"12425 Victory Bl.",\
+          "PROJECT_DESCRIPTION":"80-room four-story hotel.",\
+          "APN":"1234-567-890",\
+          "LAND_USE_HOTEL":true,\
+          "UNITS_GUEST":"80",\
+          "PARK_SPACES":"76",\
+          "STRATEGY_HOV_3":true,\
+          "STRATEGY_BIKE_4":true,\
+          "STRATEGY_INFO_2":true}',
+        calculationId: 1,
+        dateCreated: '2020-11-12T00:38:42.763Z',
+        dateModified: '2020-11-12T00:39:04.436Z',
+        description: '80-room four-story hotel.',
+        loginId: 37, // ladot
+        firstName: 'LA',
+        lastName: 'DOT',
+      },
+    });
+  };
+
   beforeEach(() => {
     window.localStorage.setItem('termsAndConditions', 'Accepted');
-    cy.loginAs('ladot');
-
-    cy.visit('/projects');
-    cy.get('table').find('tr td a').first().click();
+    cy.loginAs('ladot').then(cy.resetProjects);
   });
 
   it('shows up disabled on every page when there are not changes', () => {
-    cy.findByRole('button', { name: /Save Project/i }).should('be.disabled');
-    cy.findByTestId('rightNavArrow').click();
+    postNewProject().then((res) => {
+      cy.visit('/projects');
+      cy.findByText('Some Project Hotel').should('be.visible').click();
 
-    cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
-    cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
 
-    cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
-    cy.findByTestId('rightNavArrow').click();
+      cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
 
-    cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
-    cy.findByTestId('rightNavArrow').click();
+      cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
 
-    cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
-    cy.findByTestId('rightNavArrow').click();
+      cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
+
+      cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
+
+      cy.findByTestId('rightNavArrow').click();
+      cy.findByRole('button', { name: 'Save Project' }).should('be.disabled');
+    });
   });
 });
