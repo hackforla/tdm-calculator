f03b70326fba2c90ed14ddedc2fadc98214a01df
John Darragh <darragh@entrotech.net>
2022-02-09 09:08:36 -0800

    Set link column to null in db for AIN & Bike Share memberships to cause the labels to not be rendered as hyperlinks in the UI

--- server/db/migration/V20220207.1702__move-help-links-inside-tooltips_1058.sql
+++ server/db/migration/V20220207.1702__move-help-links-inside-tooltips_1058.sql
@@ -1,16 +1,15 @@
 update CalculationRule set
 	description = '<div>You may  look up your Assessor&apos;s Identification number on the 
 	<a href="http://maps.assessor.lacounty.gov" target="_blank" rel="noopener noreferrer">
-	Los Angeles County Propery Assessment Information System
-	</a> portal.
-	</div>'
+	Los Angeles County Propery Assessment Information System</a> portal.
+	</div>', link = NULL
 	where calculationId = 1 and code = 'APN'
 
 update CalculationRule set
 	description = 'Offer bike share membership passes to employees and/or residents in accordance to the 
 	<a href="https://bikeshare.metro.net/for-business/" target="_blank"  rel="noopener noreferrer">
-	Bikeshare for Business </a> membership levels. 
-	Applicable for locations within 0.25 miles from an existing or planned 
+	Bikeshare for Business</a> membership levels. 
+	Applicable for locations within 0.25 miles of an existing or planned 
 	<a href="https://bikeshare.metro.net/stations" target="_blank" rel="noopener noreferrer">Bike Share Station</a>.
-	'
+	', link = NULL
 where calculationId = 1 and code = 'STRATEGY_BIKE_3'
