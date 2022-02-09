7aeb13e26cdd5fe83437cf3bad8f78d2170e7903
John Darragh <darragh@entrotech.net>
2022-02-08 17:28:59 -0800

    Adjustments to wording of tooltips for AIN and Bikeshare Memberships

--- server/db/migration/V20220207.1702__move-help-links-inside-tooltips_1058.sql
+++ server/db/migration/V20220207.1702__move-help-links-inside-tooltips_1058.sql
@@ -1,7 +1,11 @@
 update CalculationRule set
-	description = '<div>You may  look up your Assessor&apos;s Identification number <a href="http://maps.assessor.lacounty.gov" target="_blank" rel="noopener noreferrer">here</a></div>'
+	description = '<div>You may  look up your Assessor&apos;s Identification number on the <a href="http://maps.assessor.lacounty.gov" target="_blank" rel="noopener noreferrer">Los Angeles County Propery Assessment Information System</a>portal.</div>'
 	where calculationId = 1 and code = 'APN'
 
 update CalculationRule set
-	description = 'Offer bike share membership passes to employees and/or residents in accordance to the Bikeshare for Business  membership levels  (applicable for locations within 0.25 miles from an existing or planned bike share station - (<a href="https://bikeshare.metro.net/for-business/" target="_blank" rel="noopener noreferrer">Bike Share Location Map</a>).'
+	description = 'Offer bike share membership passes to employees and/or residents in accordance to the 
+	<a href="https://bikeshare.metro.net/for-business/" target="_blank"  rel="noopener noreferrer">
+	Bikeshare for Business </a> membership levels 
+	(applicable for locations within 0.25 miles from an existing or planned bike share station per the map at - 
+	(<a href="https://bikeshare.metro.net/stations" target="_blank" rel="noopener noreferrer">Bike Share Station Map</a>).'
 where calculationId = 1 and code = 'STRATEGY_BIKE_3'
