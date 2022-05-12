d630cda413e55567404c8a8f7f7e397dc4eb3700
James Higgs <james.y.higgs@gmail.com>
2022-05-11 20:40:58 -0700

    made edits based on Fangs comments for encouragement program strategy

--- server/db/migration/V20220413.2005__adjust_tooltips_1059.sql
+++ server/db/migration/V20220413.2005__adjust_tooltips_1059.sql
@@ -27,3 +27,26 @@ description =
 	</div>'
 
 where calculationId = 1 and code = 'STRATEGY_AFFORDABLE'
+
+/* Encouragement Program Strategy */
+update CalculationRule set 
+ description = '<ol style="margin-left:-1.6em;margin-top:0;margin-bottom:0">
+	<li> Education, Marketing and Outreach: Offer new employees and residents a 
+	packet of materials and/or provide personal consultation detailing sustainable (non-SOV) 
+	travel options. These materials or consultation must be available on an ongoing 
+	basis and/or on permanent online channels. Packet must include the 
+	distribution of a one Metro TAP card pre-loaded with a trip, to each 
+	employee or residential unit.</li>
+	<li style="margin-top:0.5em"> Travel Behavior Change Program: A multi-faceted program 
+	involving two-way communication campaigns and travel feedback that 
+	actively engages participants to target individual attitudes, goals, 
+	and travel behaviors to alter their travel choices and habits. 
+	Program must include the distribution of a one Metro TAP card pre-loaded with a trip
+	to each employee or residential unit. Selection of this strategy requires a 
+	coordinator to manage the program, and ensure communication is available to 
+	all regular occupants of a site with a special focus on new occupants and/or employees. </li>
+	<li style="margin-top:0.5em"> 
+		NOTE: Cannot be combined with Required Trip-Reduction Program strategy.
+	</li>
+</ol>'
+where calculationId = 1 and code = 'STRATEGY_INFO_3'
