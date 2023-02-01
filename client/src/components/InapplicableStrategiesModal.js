6db0dad934de314e72032be20597720546a70942
James Higgs <jameshiggs@James-MacBook-Pro.local>
2023-01-31 21:07:32 -0800

    230131

--- client/src/components/InapplicableStrategiesModal.js
+++ client/src/components/InapplicableStrategiesModal.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import Modal from "react-modal";
 import Button from "./Button/Button";
 import { createUseStyles } from "react-jss";
-import WarningIcon from "../images/warning-icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 const useStyles = createUseStyles({
   overlay: {
@@ -20,20 +21,20 @@ const useStyles = createUseStyles({
     bottom: "0px"
   },
   content: {
-    padding: "60px",
+    padding: "50px",
     backgroundColor: "#ffffff",
-    boxShadow: "0px 5px 10px rgba(0, 46, 109, 0.2)"
+    boxShadow: "0px 5px 10px rgba(0, 46, 109, 0.2)",
+    width: "40%",
+    borderRadius: "5px"
   },
   title: {
     textAlign: "center"
   },
-  warningWrapper: {
-    color: "#B64E38",
-    display: "flex",
-    alignItems: "center"
+  deselectedWrapper: {
+    textAlign: "center"
   },
-  warningMessage: {
-    verticalAlign: "middle"
+  deselectedAlign: {
+    lineHeight: "40px"
   },
   modalActions: {
     display: "flex",
@@ -50,26 +51,31 @@ const InapplicableStrategiesModal = (
     <Modal
       isOpen={inapplicableStrategiesModalOpen}
       onRequestClose={() => setInapplicableStrategiesModal(false)}
-      contentLabel="Navigation Confirmation"
+      contentLabel="Inapplicable Strategies"
       overlayClassName={classes.overlay}
       className={classes.content}
       shouldFocusAfterRender={false}
     >
-      <p className={classes.warningWrapper}>
-        <img src={WarningIcon} alt="Warning" />
-        <span className={classes.warningMessage}>
-          &nbsp; This will permanently delete any unsaved projects or changes to
-          project.
-        </span>
-      </p>
+      <div className={classes.deselectedWrapper}>
+        <FontAwesomeIcon
+          icon={faTriangleExclamation}
+          style={{ color: "#E46247", height: "80px" }}
+          alt="Warning"
+        />
+        <h2 className={classes.deselectedAlign}>
+          Due to changes made to the project specifications, one or more TDM
+          strategies are no longer applicable and have been automatically
+          de-selected
+        </h2>
+      </div>
       <div className={classes.modalActions}>
         <Button
-          color="colorError"
+          color="colorDeselect"
           id="modalProceed"
           data-testid="transitionProceed"
           onClick={() => setInapplicableStrategiesModal(false)}
         >
-          Proceed
+          Okay, I Understand
         </Button>
       </div>
     </Modal>
