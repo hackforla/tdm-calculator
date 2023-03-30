ac414e4edc5c01cbb52cf87c8c1a7df7f9f11007
John Darragh <darragh@entrotech.net>
2023-03-29 18:38:11 -0700

    Auto-reconcile Affordable Housing settings on p2 and p4 (#1350)
    * Auto-reconcile Affordable Housing settings on p2 and p4
    
    * revert client/package-lock.json
    
    * revert package-lock.json

--- client/src/components/TdmCalculationContainer.js
+++ client/src/components/TdmCalculationContainer.js
@@ -375,6 +375,25 @@ export function TdmCalculationContainer({
         if (value === "2") {
           formInputs["STRATEGY_CAR_SHARE_ELECTRIC"] = true;
         }
+        break;
+      case "STRATEGY_AFFORDABLE":
+        // When the Strategy Affordable housing is set to 100% Affordable,
+        // The 100% Affordable Housing Input should be set to true
+        if (value === "4") {
+          formInputs["AFFORDABLE_HOUSING"] = true;
+        } else {
+          formInputs["AFFORDABLE_HOUSING"] = false;
+        }
+        break;
+      case "AFFORDABLE_HOUSING":
+        if (value === true) {
+          formInputs["STRATEGY_AFFORDABLE"] = "4";
+        } else {
+          if (formInputs["STRATEGY_AFFORDABLE"] === "4") {
+            formInputs["STRATEGY_AFFORDABLE"] = "";
+          }
+        }
+        break;
     }
   };
 
