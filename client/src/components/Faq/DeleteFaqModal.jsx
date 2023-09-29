6c84edaff0119a79663a2449d94d873e1f8c601b
Shane Sweeney <ssweeney@irv-mac-ssween2.lan>
2023-09-29 14:15:59 -0700

    FAQ Dialog styling refactor

--- client/src/components/Faq/DeleteFaqModal.jsx
+++ client/src/components/Faq/DeleteFaqModal.jsx
@@ -2,20 +2,54 @@ import React from "react";
 import PropTypes from "prop-types";
 import ModalDialog from "../UI/AriaModal/ModalDialog";
 import Button from "../Button/Button";
+import WarningIcon from "../../images/warning-icon.png";
+import { createUseStyles } from "react-jss";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const DeleteFaqModal = ({ isModalOpen, closeModal, handleDelete }) => {
+const useStyles = createUseStyles(theme => ({
+  buttonFlexBox: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    margin: 0
+  },
+  modalHeader: theme.typography.heading1,
+  modalSubHeader: theme.typography.subHeading,
+  instruction: {
+    fontSize: "20px",
+    lineHeight: "32px",
+    textAlign: "center",
+    color: "#B64E38",
+    "& span": {
+      fontStyle: "italic"
+    }
+  },
+  warningIcon: {
+    margin: "0 10px"
+  }
+}));
+
+const DeleteFaqModal = ({ isModalOpen, closeModal, handleDelete, isFaq }) => {
+  const classes = useStyles();
+  const type = isFaq ? "FAQ" : "Category";
   return (
-    <ModalDialog mounted={isModalOpen} underlayClickExits={false} omitCloseBox>
-      <div>
-        <p>Are you sure you would like to delete this?</p>
-        <div className="buttonFlexBox">
-          <Button variant="text" onClick={closeModal}>
-            Cancel
-          </Button>
-          <Button variant="warning" type="submit" onClick={handleDelete}>
-            Delete
-          </Button>
-        </div>
+    <ModalDialog mounted={isModalOpen} onClose={closeModal}>
+      <div className={classes.modalHeader} style={{ marginBottom: "1.5rem" }}>
+        <FontAwesomeIcon icon={faTrashCan} />
+        {` Delete ${type}`}
+      </div>
+      <div className={classes.modalSubHeader}>
+        <img src={WarningIcon} className={classes.warningIcon} alt="Warning" />
+        {`Are you sure you want to permanently delete the ${type}?`}
+      </div>
+      <div className={classes.buttonFlexBox}>
+        <Button onClick={closeModal} variant="text" id="cancelButton">
+          Cancel
+        </Button>
+        <Button onClick={handleDelete} variant="contained" color={"colorError"}>
+          Delete
+        </Button>
       </div>
     </ModalDialog>
   );
@@ -24,7 +58,8 @@ const DeleteFaqModal = ({ isModalOpen, closeModal, handleDelete }) => {
 DeleteFaqModal.propTypes = {
   isModalOpen: PropTypes.bool,
   closeModal: PropTypes.func,
-  handleDelete: PropTypes.func
+  handleDelete: PropTypes.func,
+  isFaq: PropTypes.bool
 };
 
 export default DeleteFaqModal;
