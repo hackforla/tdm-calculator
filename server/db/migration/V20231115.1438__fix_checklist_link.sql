da23d8d19ef342399e97b49ddf17c3e4167ab228
ZekeAranyLucas <zeke@aranylucas.com>
2023-11-20 15:43:35 -0800

    TODO: update backend faq for checklist later

--- server/db/migration/V20231115.1438__fix_checklist_link.sql
+++
@@ -1,7 +0,0 @@
-INSERT Faq (question, answer, displayOrder, faqCategoryId)
-VALUES(
-'What do I need to prepare to get started?',
-'<div>You may refer to the hyperlinked <a href="/faqs/true">checklist</a> at the bottom right of your browser window.</div>',
-20,
-2
-)
