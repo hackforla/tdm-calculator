a019f169b51251a63b34f30bd0a7c207eb1d6536
Tomomi-K1 <tomomi.music@gmail.com>
2024-01-03 21:01:51 -0800

    bug fix: do not show relevant projects when user has no projects

--- client/src/components/Feedback/FeedbackPage.js
+++ client/src/components/Feedback/FeedbackPage.js
@@ -233,7 +233,7 @@ const FeedbackPage = ({ account }) => {
                   />
                 </label>
               </div>
-              {account && account.id ? (
+              {account && account.id && projects.length !== 0 ? (
                 <ProjectList
                   key={JSON.stringify(projects, null, 2)}
                   projects={projects}
