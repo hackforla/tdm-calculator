8800d80f5294b375eca7ac1d9308be845b093803
John Darragh <darragh@entrotech.net>
2024-02-01 12:02:12 -0800

    Change client settings to Angeleno UAT

--- client/src/okta.config.js
+++ client/src/okta.config.js
@@ -3,12 +3,12 @@ const OKTA_TESTING_DISABLEHTTPSCHECK = true;
 const REDIRECT_URI = `${window.location.origin}/login/callback`;
 
 // Dev Okta
-const ISSUER = "https://dev-50564150.okta.com/oauth2/default";
-const CLIENT_ID = "0oaehwjjhx70xRh0O5d7";
+// const ISSUER = "https://dev-50564150.okta.com/oauth2/default";
+// const CLIENT_ID = "0oaehwjjhx70xRh0O5d7";
 
 // UAT Okta
-// const ISSUER = "https://lacitypublic.oktapreview.com/oauth2/default"
-// const CLIENT_ID = "0oabp5dgnwuGTqt5n1d7"
+const ISSUER = "https://lacitypublic.oktapreview.com/oauth2/default";
+const CLIENT_ID = "0oabp5dgnwuGTqt5n1d7";
 
 // eslint-disable-next-line
 export default {
