8a295b9ffed8fc31719af7c4f77f74073e31df2b
Iris Lee <lee.iris.s@gmail.com>
2024-02-01 20:05:30 -0800

    1573 clean up faq (#1598)
    * comments
    
    * cleanup
    
    * delete faq

--- server/app/controllers/faqCategory.controller.js
+++ server/app/controllers/faqCategory.controller.js
@@ -2,16 +2,7 @@ const faqCategoryService = require("../services/faqCategory.service");
 
 const get = async (req, res) => {
   try {
-    const response = await faqCategoryService.getFaqCategory();
-    res.json(response);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-
-const getById = async (req, res) => {
-  try {
-    const response = await faqCategoryService.getFaqCategoryById(req.params.id);
+    const response = await faqCategoryService.getFaqCategories();
     res.json(response);
   } catch (err) {
     res.status(500).send(err);
@@ -20,15 +11,6 @@ const getById = async (req, res) => {
 
 const post = async (req, res) => {
   try {
-    await faqCategoryService.postFaqCategory(req.body);
-    res.sendStatus(201);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-
-const postAll = async (req, res) => {
-  try {
     await faqCategoryService.postCategories(req.body);
     res.sendStatus(201);
   } catch (err) {
@@ -36,29 +18,7 @@ const postAll = async (req, res) => {
   }
 };
 
-const put = async (req, res) => {
-  try {
-    await faqCategoryService.putFaqCategoryById(req.body);
-    res.sendStatus(200);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-
-const del = async (req, res) => {
-  try {
-    await faqCategoryService.deleteFaqCategory(req.params.id);
-    res.sendStatus(200);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-
 module.exports = {
   get,
-  getById,
-  post,
-  postAll,
-  put,
-  del
+  post
 };
