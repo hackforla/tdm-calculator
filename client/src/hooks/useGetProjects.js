6b4eb17efd787e53b69b75e7875c81f928de9fc0
Jonathanko52 <jonathan.ko523@gmail.com>
2024-03-27 18:57:15 -0700

    Date comparisons

--- client/src/hooks/useGetProjects.js
+++ client/src/hooks/useGetProjects.js
@@ -10,6 +10,16 @@ const useProjects = handleError => {
       if (result.data === "" || result.data === false) {
         setProjects([]);
       } else {
+        console.log(result.data);
+        console.log(
+          "CURRENT",
+          (new Date() - new Date(result.data[0].dateCreated)) /
+            1000 /
+            60 /
+            60 /
+            24
+        );
+
         setProjects(result.data);
       }
     } catch (err) {
