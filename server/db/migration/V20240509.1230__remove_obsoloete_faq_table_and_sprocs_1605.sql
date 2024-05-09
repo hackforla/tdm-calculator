5412719f154fc8ce83351b6e4206db3714f7e042
John Darragh <darragh@entrotech.net>
2024-05-09 12:43:40 -0700

    1605 remove unused faq sprocs (#1699)
    * Fix vulnerability in accountUpdate
    
    * Secure /api/accounts/:id/updateAccount endpoint
    
    * Remove Faq table and realted sprocs

---
+++ server/db/migration/V20240509.1230__remove_obsoloete_faq_table_and_sprocs_1605.sql
@@ -0,0 +1,7 @@
+drop proc if exists Faq_Insert;
+drop proc if exists Faq_Update;
+drop proc if exists Faq_Delete;
+drop proc if exists Faq_SelectAll;
+drop proc if exists Faq_SelectById;
+
+drop table if exists Faq;
