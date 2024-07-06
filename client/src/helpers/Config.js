6b8767aa698e83be691838c41c1ddbbb055a5ebb
John Darragh <darragh@entrotech.net>
2024-07-05 22:16:32 -0400

    Remove dead code for obsolete okta wic integration

--- client/src/helpers/Config.js
+++ client/src/helpers/Config.js
@@ -15,20 +15,3 @@ export const getConfigs = async () => {
     return [err];
   }
 };
-
-// export const getOidc = async () => {
-//   const configs = await getConfigs();
-//   const clientId = configs.OKTA_CLIENT_ID;
-//   const issuer = configs.OKTA_ISSUER;
-//   const disableHttpsCheck = configs.OKTA_TESTING_DISABLE_HTTPS_CHECK || "T";
-//   const oidc = {
-//     clientId,
-//     issuer,
-//     redirectUri,
-//     scopes: ["openid", "profile", "email"],
-//     pkce: true,
-//     disableHttpsCheck
-//   };
-
-//   return { configs, oidc };
-// };
