c1d3f896f644e8a5edb6865da09e29683f92f81f
Jonathanko52 <jonathan.ko523@gmail.com>
2024-08-28 14:10:20 -0700

    initial push

---
+++ server/db/migration/V20240822.1446__add_project_submit-sproc.sql
@@ -0,0 +1,23 @@
+CREATE OR ALTER PROC [dbo].[Project_Submit]
+	@id int
+	, @loginId int
+AS
+BEGIN
+	DECLARE @rc int
+	SELECT @rc = count(*) FROM Project p WHERE p.id = @id AND p.loginId = @loginId
+	IF (@rc = 0)
+	BEGIN
+		RETURN 1 /* project is not owned by @loginId - throw error */
+	END
+
+	IF EXISTS (SELECT count(*) FROM Project p WHERE p.id = @id and p.dateSnapshotted IS NULL)
+	BEGIN
+		RETURN 2 /* project is not a snapshot */
+	END
+
+	UPDATE Project SET 
+		dateSubmitted = getutcdate()
+	WHERE Project.id = @id
+
+END
+GO
