hackforla/tdm-calculator

commit 8fc059dec96b737d827951f9eb75f4e75449f86f

1842 add expiration date/time to user account (#1904)
* add expiration to user structure, check expiration every 10s, set account to null upon expiration

* fix error in expires property of cookie

* changes in various files required to prevent attempted access of properties of null account object

* 5 sec interval for checking user account expiration

* remove console log statements for production

client/src/components/Projects/ProjectsPage.jsx
@@ -948,7 +948,11 @@ const ProjectsPage = ({ contentContainerRef }) => {
                           handleHide={handleHide}
                           handleCheckboxChange={handleCheckboxChange}
                           checkedProjectIds={checkedProjectIds}
-                          isAdmin={userContext.account?.isAdmin}
+                          isAdmin={
+                            UserContext.account
+                              ? UserContext.account.isAdmin
+                              : false
+                          }
                           droOptions={droOptions}
                           onDroChange={handleDroChange} // Pass the DRO change handler
                           onAdminNoteUpdate={handleAdminNoteUpdate} // Pass the admin note update handler

client/src/components/Authorization/UpdateAccount.jsx
@@ -24,8 +24,8 @@ const UpdateAccount = props => {
   const classes = useStyles();
   const params = useParams();
   const initialValues = {
-    firstName: account.firstName || "",
-    lastName: account.lastName || "",
+    firstName: account?.firstName || "",
+    lastName: account?.lastName || "",
     email: params.email || ""
   };
 

client/src/components/Layout/NavBarLogin.jsx
@@ -9,6 +9,7 @@ import NavBarToolTip from "./NavBarToolTip";
 const NavBarLogin = ({ classes, handleHamburgerMenuClick }) => {
   const userContext = useContext(UserContext);
   const account = userContext.account;
+
   const [isCalculation, setIsCalculation] = useState(false);
 
   const location = useLocation();

client/src/components/Layout/TdmAuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import UserContext from "../../contexts/UserContext";
 
@@ -18,6 +18,21 @@ const getUserFromLocalStorage = () => {
 
 const TdmAuthProvider = ({ children }) => {
   const [account, setAccount] = useState(getUserFromLocalStorage());
+
+  const checkUserExpiration = () => {
+    if (account) {
+      const now = new Date();
+      const expirationDate = new Date(account.expiration);
+      if (now > expirationDate) {
+        updateAccount(null);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(checkUserExpiration, 5000); // Poll every 5 seconds
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  });
 
   const updateAccount = userAccount => {
     /*

client/src/components/ProjectWizard/TdmCalculationContainer.jsx
@@ -79,7 +79,7 @@ export function TdmCalculationContainer({ contentContainerRef }) {
     try {
       let projectResponse = null;
       let inputs = {};
-      if (Number(projectId) > 0 && account.id) {
+      if (Number(projectId) > 0 && account?.id) {
         projectResponse = await projectService.getById(projectId);
 
         // setLoginId(projectResponse.data.loginId);

client/src/components/Projects/FilterDrawer.jsx
@@ -170,7 +170,7 @@ const FilterPopup = ({
           onChange={e => handleChange(e, "address")}
           className={classes.textInput}
         />
-        {account.isAdmin && (
+        {account?.isAdmin && (
           <>
             <h4 className={classes.minorHeading}>Author</h4>
             <input

client/src/components/Projects/MultiProjectToolbarMenu.jsx
@@ -60,7 +60,7 @@ const MultiProjectToolbarMenu = ({
   ) {
     project = checkedProjectsStatusData;
   }
-  const isProjectOwner = account.id === project?.loginId;
+  const isProjectOwner = account ? account.id === project?.loginId : false;
 
   const isBtnDisabled = (projProp, criteriaProp) => {
     const sameDateVals = checkedProjectsStatusData[projProp] !== false;

client/src/components/Projects/ProjectContextMenu.jsx
@@ -63,7 +63,7 @@ const ProjectContextMenu = ({
 
   return (
     <ul className={classes.list}>
-      {project.dateSnapshotted && project.loginId == account.id ? (
+      {project.dateSnapshotted && project.loginId == account?.id ? (
         <li
           className={classes.listItem}
           onClick={() => handleClick(handleRenameSnapshotModalOpen)}
@@ -76,7 +76,7 @@ const ProjectContextMenu = ({
         </li>
       ) : null}
 
-      {project.dateSnapshotted && project.loginId !== account.id ? (
+      {project.dateSnapshotted && project.loginId !== account?.id ? (
         <li className={classes.listItemDisabled}>
           <MdEdit
             className={classes.listItemIcon}
@@ -86,7 +86,7 @@ const ProjectContextMenu = ({
         </li>
       ) : null}
 
-      {!project.dateSnapshotted && project.loginId == account.id ? (
+      {!project.dateSnapshotted && project.loginId == account?.id ? (
         <li
           className={classes.listItem}
           onClick={() => handleClick(handleSnapshotModalOpen)}
@@ -129,7 +129,7 @@ const ProjectContextMenu = ({
         />
         Duplicate
       </li>
-      {project.loginId !== account.id ? null : (
+      {project.loginId !== account?.id ? null : (
         <li
           onClick={() => handleClick(handleHide)}
           className={classes.listItem}
@@ -153,7 +153,7 @@ const ProjectContextMenu = ({
           )}
         </li>
       )}
-      {project.loginId !== account.id ? null : (
+      {project.loginId !== account?.id ? null : (
         <li
           onClick={() => handleClick(handleDeleteModalOpen)}
           className={classes.listItem}

commit 47d2412dd7cf44102d0074ea407a2a7d6309a6c9

Fixed bug introduced in PR #1904 that caused Admin Notes, Date Admin Modified cols to be hidden

client/src/components/Projects/ProjectsPage.jsx
@@ -169,6 +169,7 @@ const ProjectsPage = ({ contentContainerRef }) => {
   const [droOptions, setDroOptions] = useState([]);
   const [droNameMap, setDroNameMap] = useState({});
   const projectsPerPage = perPage;
+  const isAdmin = userContext.account?.isAdmin || false;
 
   useEffect(() => {
     // Check if the user is an admin
@@ -948,18 +949,12 @@ const ProjectsPage = ({ contentContainerRef }) => {
                           handleHide={handleHide}
                           handleCheckboxChange={handleCheckboxChange}
                           checkedProjectIds={checkedProjectIds}
-                          isAdmin={
-                            UserContext.account
-                              ? UserContext.account.isAdmin
-                              : false
-                          }
+                          isAdmin={isAdmin}
                           droOptions={droOptions}
                           onDroChange={handleDroChange} // Pass the DRO change handler
                           onAdminNoteUpdate={handleAdminNoteUpdate} // Pass the admin note update handler
                           droName={
-                            userContext.account?.isAdmin
-                              ? null
-                              : droNameMap[project.droId] || "N/A"
+                            isAdmin ? null : droNameMap[project.droId] || "N/A"
                           } // Pass the droName
                         />
                       ))