18a9bb0a9e0c61c863dedf1315431776f29a6ba1
John Darragh <darragh@entrotech.net>
2024-11-05 20:17:13 -0800

    Removed superfluous db migration

--- server/db/migration/V20241008.1761__add_projectShare_test.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO dbo.ProjectShare (email, projectId)
-VALUES 
-('testing1@email.com', 2),
-('testing3@email.com', 2),
-('testing5@email.com', 2),
-('testing7@email.com', 2),
-('testing1@email.com', 3),
-('testing2@email.com', 3),
-('testing3@email.com', 4);
