58b4c3ec707e57efa583b5773542983960893a8a
heejung1180 <heejung1180@outlook.com>
2024-11-11 21:56:52 -0800

    move Search box and add Reset Filter button with number of sorted projects

--- client/src/components/Projects/ProjectsPage.jsx
+++ client/src/components/Projects/ProjectsPage.jsx
@@ -283,6 +283,7 @@ const ProjectsPage = ({ contentContainerRef }) => {
     startDateModifiedAdmin: null,
     endDateModifiedAdmin: null
   });
+
   const [filterCollapsed, setFilterCollapsed] = useState(true);
   const checkedProjectsStatusData = useCheckedProjectsStatusData(
     checkedProjectIds,
@@ -751,6 +752,33 @@ const ProjectsPage = ({ contentContainerRef }) => {
     return true;
   };
 
+  const resetFiltersSort = () => {
+    setCriteria({
+      type: "all",
+      status: "active",
+      visibility: "visible",
+      name: "",
+      address: "",
+      author: "",
+      alternative: "",
+      dro: "",
+      startDateCreated: null,
+      endDateCreated: null,
+      startDateModified: null,
+      endDateModified: null,
+      nameList: [],
+      addressList: [],
+      alternativeList: [],
+      authorList: [],
+      droList: [],
+      adminNotes: "",
+      startDateModifiedAdmin: null,
+      endDateModifiedAdmin: null
+    });
+    setCheckedProjectIds([]);
+    setSelectAllChecked(false);
+  };
+
   const headerData = [
     {
       id: "checkAllProjects",
@@ -873,7 +901,7 @@ const ProjectsPage = ({ contentContainerRef }) => {
                 style={{
                   display: "flex",
                   flexDirection: "row",
-                  justifyContent: "space-between"
+                  justifyContent: "flex-start"
                 }}
               >
                 <MemoizedMultiProjectToolbar
@@ -889,7 +917,10 @@ const ProjectsPage = ({ contentContainerRef }) => {
                   style={{
                     display: "flex",
                     flexDirection: "row",
-                    alignSelf: "flex-end"
+                    alignSelf: "flex-end",
+                    marginLeft: "20px",
+                    justifyContent: "space-between",
+                    width: "80%"
                   }}
                 >
                   <div className={classes.searchBarWrapper}>
@@ -904,16 +935,24 @@ const ProjectsPage = ({ contentContainerRef }) => {
                     />
                     <MdOutlineSearch className={classes.searchIcon} />
                   </div>
-                  {filterCollapsed ? (
+                  <div>
                     <button
-                      alt="Show Filter Criteria"
-                      style={{ backgroundColor: "#0F2940", color: "white" }}
-                      onClick={() => setFilterCollapsed(false)}
+                      onClick={resetFiltersSort}
+                      style={{ height: "40px" }}
                     >
-                      <MdFilterAlt style={{ marginRight: "0.5em" }} />
-                      Filter By
+                      RESET FILTERS/SORT ({sortedProjects.length})
                     </button>
-                  ) : null}
+                    {filterCollapsed ? (
+                      <button
+                        alt="Show Filter Criteria"
+                        style={{ backgroundColor: "#0F2940", color: "white" }}
+                        onClick={() => setFilterCollapsed(false)}
+                      >
+                        <MdFilterAlt style={{ marginRight: "0.5em" }} />
+                        Filter By
+                      </button>
+                    ) : null}
+                  </div>
                 </div>
               </div>
               <div className={classes.tableContainer}>
