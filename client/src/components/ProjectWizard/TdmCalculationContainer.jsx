68abf60a8374701f426c92d8147949b0b63a8e26
John Darragh <darragh@entrotech.net>
2025-03-20 10:00:29 -0700

    When clearing page 4, also clear user-defined strategy comment

--- client/src/components/ProjectWizard/TdmCalculationContainer.jsx
+++ client/src/components/ProjectWizard/TdmCalculationContainer.jsx
@@ -432,6 +432,11 @@ export function TdmCalculationContainer({ contentContainerRef }) {
         if (updateInputs[rules[i].code]) {
           updateInputs[rules[i].code] = null;
         }
+        // In addition to the rule value, also clear
+        // the associated comment, if any
+        if (updateInputs[rules[i].code + "_comment"]) {
+          updateInputs[rules[i].code + "_comment"] = null;
+        }
       }
     }
     if (filterRules === filters.projectDescriptionRules) {
