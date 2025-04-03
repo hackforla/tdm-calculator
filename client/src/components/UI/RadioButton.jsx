d9951815ba4b6dd1a9bdf73a5878ac449b12cc9e
Terence Lo <terencelomoa@gmail.com>
2025-04-02 21:55:09 -0700

    styled RadioButton

--- client/src/components/UI/RadioButton.jsx
+++ client/src/components/UI/RadioButton.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  radioButtonInput: {
+    verticalAlign: "middle",
+    padding: "0",
+    marginRight: "5px",
+    appearance: "none",
+    WebkitAppearance: "none",
+    borderRadius: "50%",
+    boxShadow: "0 0 0 1px #939393",
+    width: "1em",
+    height: "1em",
+    "&:hover": {
+      boxShadow: "0 0 0 1px #000000"
+    },
+    "&:checked": {
+      border: "3px solid #FFF",
+      background: "#1967D2",
+      boxShadow: "0 0 0 1px #1967D2",
+      "&:hover": {
+        background: "#1450A4",
+        boxShadow: "0 0 0 1px #1450A4"
+      }
+    }
+  }
+});
 
 const RadioButton = ({ label, value, checked, onChange }) => {
+  const classes = useStyles();
+
   return (
     <label style={{ margin: "0.5em" }}>
       <input
-        style={{ verticalAlign: "middle" }}
+        className={classes.radioButtonInput}
         type="radio"
         value={value}
         checked={checked}
         onChange={onChange}
       />
-      <span style={{ verticalAlign: "middle" }}> {label}</span>
+      <span style={{ verticalAlign: "middle" }}>{label}</span>
     </label>
   );
 };
