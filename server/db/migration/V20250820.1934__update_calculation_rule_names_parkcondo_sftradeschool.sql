dc3f111abd64938ce2f644c49de3096b6d8a2991
Tony-Villa <tonyvilla77@gmail.com>
2025-09-03 17:58:46 -0700

    fix: revise text on page 2 of calc

---
+++ server/db/migration/V20250820.1934__update_calculation_rule_names_parkcondo_sftradeschool.sql
@@ -0,0 +1,7 @@
+UPDATE [CalculationRule]
+SET name = '..... Required Number of Parking Spaces'
+WHERE id = 238;
+
+UPDATE [CalculationRule]
+SET name = '..... Classroom or Assembly Area'
+WHERE id = 104;
