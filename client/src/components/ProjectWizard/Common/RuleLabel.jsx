8b85fa8af6108a23f72a78d5ca1f4e705667fbb2
Michael Hy <hymike88@gmail.com>
2025-09-22 13:45:47 -0700

    show add icon only for admin users

--- client/src/components/ProjectWizard/Common/RuleLabel.jsx
+++ client/src/components/ProjectWizard/Common/RuleLabel.jsx
@@ -211,14 +211,13 @@ const RuleLabel = ({
           <MdInfo className={classes.infoIcon} />
         </span>
       ) : (
-        isAdmin && (
-          <span
-            className={clsx("fa-layers fa-fw", classes.iconContainer)}
-            onClick={addDescriptionHandler}
-          >
-            <MdAddCircle className={classes.infoIcon} />
-          </span>
-        )
+        <span
+          className={clsx("fa-layers fa-fw", classes.iconContainer)}
+          style={showDescription ? { visibility: "visible" } : {}}
+          onClick={addDescriptionHandler}
+        >
+          {isAdmin ? <MdAddCircle className={classes.infoIcon} /> : null }
+        </span>
       )}
     </div>
   );
