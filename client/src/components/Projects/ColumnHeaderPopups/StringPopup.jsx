ee58087f919570d3b74fa46dab11c2caf7c09ba1
nathnael <134805961+nchanyal@users.noreply.github.com>
2025-10-05 13:58:15 -0400

    Capitalize the ‘clear’ option in StringPopup

--- client/src/components/Projects/ColumnHeaderPopups/StringPopup.jsx
+++ client/src/components/Projects/ColumnHeaderPopups/StringPopup.jsx
@@ -204,7 +204,7 @@ const TextPopup = ({
           className={classes.toggleButton}
           onClick={() => setSelectedListItems([])}
         >
-          clear
+          Clear
         </button>
         <div>{`${selectedListItems.length}  selected`}</div>
       </div>
