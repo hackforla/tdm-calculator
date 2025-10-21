1e48a009d44c799c95eaabd36cb5784a07769eae
Michael Hy <hymike88@gmail.com>
2025-10-21 12:34:51 -0700

    install Dompurify to the server and add middleware function

---
+++ server/middleware/sanitize-html.js
@@ -0,0 +1,13 @@
+import { JSDOM } from 'jsdom';
+import DOMPurify from 'dompurify';
+
+const window = new JSDOM('').window;
+const purify = DOMPurify(window);
+
+export const sanitizeHtml = (dirty) => {
+    if (!dirty || typeof dirty !== 'string') return dirty
+
+    return purify.sanitize(dirty);
+
+}
+
