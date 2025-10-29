e49c0d1cc998061a76fc59febff6701a8cd0a97d
Michael Hy <hymike88@gmail.com>
2025-10-29 16:15:57 -0700

    *Implement changes for sanitizeHTML to include defense against bad inline CSS code while allowing non-harmful ones thru #Created unit test for the main requirements and additional coverages.

--- client/src/helpers/SanitizeRichText.js
+++ client/src/helpers/SanitizeRichText.js
@@ -1,5 +1,28 @@
 import DomPurify from "dompurify";
 
+// Hook to allow safe CSS while blocking dangerous ones
+DomPurify.addHook("uponSanitizeAttribute", (node, data) => {
+  if (data.attrName === "style" && typeof data.attrValue === "string") {
+    const dangerousPatterns = [
+      /javascript:/gi,
+      /expression\(/gi,
+      /vbscript:/gi,
+      /onload\s*=/gi,
+      /onerror\s*=/gi
+    ];
+
+    const hasDangerousPattern = dangerousPatterns.some(pattern =>
+      pattern.test(data.attrValue)
+    );
+
+    if (hasDangerousPattern) {
+      data.keepAttr = false;
+    } else {
+      data.keepAttr = true;
+    }
+  }
+});
+
 export const sanitizeHtml = dirtyContent => {
   return DomPurify.sanitize(dirtyContent);
 };
