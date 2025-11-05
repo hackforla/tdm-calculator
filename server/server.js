a1454473cc2d9e474b53ba80f0f0de6745f7d5b8
John Darragh <darragh@entrotech.net>
2025-11-04 16:52:41 -0800

    Disable CSP to debug deployment

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("error-handler");
 const routes = require("./app/routes");
+// const helmet = require("helmet");
 // const pino = require("express-pino-logger")();
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+// app.use(helmet);
 // app.use(pino);
 
 if (process.env.NODE_ENV === "production") {
@@ -26,14 +28,14 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Set headers & end pre-flight requests
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self' "
-  );
-  next();
-});
+// app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self' "
+//   );
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
