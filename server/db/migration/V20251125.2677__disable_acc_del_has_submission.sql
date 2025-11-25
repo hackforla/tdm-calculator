b075d92a03f94355295793aa5bbb1cf0d5144dae
Michael Hy <hymike88@gmail.com>
2025-11-24 22:20:35 -0800

    disallow deletion of user if user has submissions.

--- server/db/migration/V20251125.2677__disable_acc_del_has_submission.sql
+++ server/db/migration/V20251125.2677__disable_acc_del_has_submission.sql
@@ -22,3 +22,28 @@ BEGIN
 
 END 
 GO
+
+CREATE OR ALTER PROCEDURE [dbo].[DeleteUserAndProjects]
+    @id INT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM [dbo].[Project]
+        WHERE [loginId] = @id
+            AND [dateSubmitted] IS NOT NULL
+            AND [dateTrashed] IS NULL
+    )
+    BEGIN 
+        RAISERROR('Cannot delete account with submissions. Account has projects that have been submitted.', 16, 1);
+        RETURN;
+    END;
+
+    DELETE FROM [dbo].[Project]
+    WHERE [loginId] = @id; 
+
+    DELETE FROM [dbo].[Login]
+    WHERE [id] = @id;
+
+END;
+GO
