c715b5fde77c2127fa56a5a327db2440ab9321f7
Tony-Villa <tonyvilla77@gmail.com>
2025-12-08 18:53:10 -0800

    fix: create project 3

--- client/src/components/ProjectWizard/RuleCalculation/RuleCalculation.jsx
+++ client/src/components/ProjectWizard/RuleCalculation/RuleCalculation.jsx
@@ -7,6 +7,7 @@ import clsx from "clsx";
 import Popup from "reactjs-popup";
 import { MdClose } from "react-icons/md";
 import { sanitizeHtml } from "helpers/SanitizeRichText";
+import { useReplaceAriaAttribute } from "hooks/useReplaceAriaAttribute";
 
 const useStyles = createUseStyles(theme => ({
   field: {
@@ -210,6 +211,16 @@ const RuleCalculation = ({
     setShowValidationErrors(true);
   };
 
+  const elementId = `context-menu-button-${code}`;
+  const popupContentId = `popup-content-${elementId}`;
+  useReplaceAriaAttribute({
+    elementId,
+    deps: [code],
+    attrToRemove: "aria-describedby",
+    attrToAdd: "aria-controls",
+    value: popupContentId
+  });
+
   // Sanitize DangerouslySetInnerHtml with DomPurify
   const cleanDescription = sanitizeHtml(description);
 
@@ -351,7 +362,7 @@ const RuleCalculation = ({
               {description ? (
                 <Popup
                   trigger={
-                    <span style={{ cursor: "pointer" }}>
+                    <span id={elementId} style={{ cursor: "pointer" }}>
                       <ToolTipIcon id={id.toString()} />
                     </span>
                   }
@@ -361,7 +372,7 @@ const RuleCalculation = ({
                 >
                   {close => {
                     return (
-                      <div style={{ margin: "1rem" }}>
+                      <div id={popupContentId} style={{ margin: "1rem" }}>
                         <button
                           style={{
                             backgroundColor: "transparent",
