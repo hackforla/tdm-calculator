229758b9318f32263d5afc61516a36b4de4bd3b3
eburdekin <eileenburdekin@proton.me>
2026-02-03 20:27:59 -0800

    add icons, remove red color from error messages

--- client/src/components/Authorization/Register.jsx
+++ client/src/components/Authorization/Register.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import * as accountService from "../../services/account.service";
 import { createUseStyles, useTheme } from "react-jss";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Link } from "react-router-dom";
 import * as Yup from "yup";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+import PropTypes from "prop-types";
+
 import Button from "../Button/Button";
 import ContentContainer from "../Layout/ContentContainer";
 
@@ -15,17 +17,67 @@ const useStyles = createUseStyles(theme => ({
   authText: {
     color: theme.colors.secondary.darkNavy
   },
-  invalidFeedback: {
+  heading1: {
+    ...theme.typography.heading1,
+    textAlign: "auto"
+  },
+
+  formGroup: {
+    marginBottom: 16
+  },
+  inputRow: {
+    display: "flex",
+    alignItems: "center",
+    gap: 8
+  },
+  inputField: {
+    flex: 1,
+    minWidth: 0
+  },
+  fieldIcon: {
+    fontSize: 18,
+    flexShrink: 0
+  },
+  validIcon: {
+    color: "green"
+  },
+  invalidIcon: {
     color: theme.colors.secondary.darkRed
   },
-  heading1: { ...theme.typography.heading1, textAlign: "auto" }
+  invalidFeedback: {
+    color: theme.colors.secondary.darkRed
+  }
 }));
 
-const Register = props => {
+const ValidationIcon = ({ touched, error, classes }) => {
+  if (!touched) return null;
+
+  return error ? (
+    <FaTimesCircle className={`${classes.fieldIcon} ${classes.invalidIcon}`} />
+  ) : (
+    <FaCheckCircle className={`${classes.fieldIcon} ${classes.validIcon}`} />
+  );
+};
+
+ValidationIcon.propTypes = {
+  touched: PropTypes.bool,
+  error: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  classes: PropTypes.shape({
+    fieldIcon: PropTypes.string.isRequired,
+    validIcon: PropTypes.string.isRequired,
+    invalidIcon: PropTypes.string.isRequired
+  }).isRequired
+};
+
+const Register = () => {
   const focusRef = useRef(null);
   const theme = useTheme();
   const classes = useStyles(theme);
   const params = useParams();
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
   const initialValues = {
     firstName: "",
     lastName: "",
@@ -34,44 +86,32 @@ const Register = props => {
     passwordConfirm: ""
   };
 
-  const [errorMsg, setErrorMsg] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  // useEffect(() => {
-  //   if (focusRef.current) {
-  //     focusRef.current.focus();
-  //   }
-  // });
-
   const registerSchema = Yup.object().shape({
     firstName: Yup.string()
       .matches(
         /^[a-zA-Z' .-]*$/,
-        "Your first name can only contain the alphabet, apostrophes, hyphens, periods, and spaces"
+        "Your first name can only contain letters and basic punctuation"
       )
       .required("First Name is required"),
     lastName: Yup.string()
       .matches(
         /^[a-zA-Z' .-]*$/,
-        "Your last name can only contain the alphabet, apostrophes, hyphens, periods, and spaces"
+        "Your last name can only contain letters and basic punctuation"
       )
       .required("Last Name is required"),
     email: Yup.string()
-      .email("Invalid email address format")
+      .email("Invalid email address")
       .required("Email is required"),
     password: Yup.string()
-      .min(
-        12,
-        "Your password must have at least 12 characters, at least one number, one capitalization, and one special character (e.g., !@#$%&*?)"
-      )
+      .min(12, "Password must be at least 12 characters")
       .matches(
-        /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*?])[A-Za-z\d@$!%*#?&]{12,}$/,
-        "Your password must have at least 12 characters, at least one number, one capitalization, and one special character (e.g., !@#$%&*?)"
+        /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*?])/,
+        "Password must include upper case, number, and special character"
       )
       .required("Password is required"),
     passwordConfirm: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords do not match")
       .required("Confirm your password")
-      .oneOf([Yup.ref("password")], "Password does not match")
   });
 
   const handleSubmit = async (
@@ -85,25 +125,25 @@ const Register = props => {
         email,
         password
       );
+
       if (response.isSuccess) {
         setSubmitted(true);
       } else if (response.code === "REG_DUPLICATE_EMAIL") {
-        setErrorMsg(`The email ${email} is already registered. Please
-          login or use the Forgot Password feature if you have
-          forgotten your password.`);
+        setErrorMsg(
+          `The email ${email} is already registered. Please log in or reset your password.`
+        );
       } else {
-        setErrorMsg(`An error occurred in sending the confirmation
-          message to ${email}. Try to log in, and follow the
-          instructions for re-sending the confirmation email.`);
+        setErrorMsg(
+          `An error occurred sending the confirmation email to ${email}.`
+        );
       }
     } catch (err) {
       setErrorMsg(err.message);
+    } finally {
       setSubmitting(false);
     }
-    // TODO: figure out if there is a scanrio where you actually
-    // want to reset the form, and move/copy the next line accordingly
-    //resetForm(initialValues);
   };
+
   return (
     <ContentContainer>
       {!submitted ? (
@@ -111,97 +151,108 @@ const Register = props => {
           <h1 className={classes.heading1}>Create a New Account</h1>
           <h3>Save your project information.</h3>
           <br />
+
           <div className="auth-form">
             <Formik
               initialValues={initialValues}
               validationSchema={registerSchema}
-              onSubmit={(values, actions) =>
-                handleSubmit(values, actions, props)
-              }
+              onSubmit={handleSubmit}
+              validateOnChange={false}
+              validateOnBlur={true}
             >
               {({ touched, errors, isSubmitting }) => (
                 <Form>
-                  <div className="form-group">
-                    <Field
-                      type="text"
-                      innerRef={focusRef}
-                      name="firstName"
-                      placeholder="First Name"
-                      className={`form-control ${
-                        touched.firstName && errors.firstName
-                          ? "is-invalid"
-                          : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="firstName"
-                      component="div"
-                      className={classes.invalidFeedback}
-                    />
+                  {/* First Name */}
+                  <div className={`form-group ${classes.formGroup}`}>
+                    <div className={classes.inputRow}>
+                      <Field
+                        type="text"
+                        innerRef={focusRef}
+                        name="firstName"
+                        placeholder="First Name"
+                        className="form-control"
+                      />
+                      <ValidationIcon
+                        touched={touched.firstName}
+                        error={errors.firstName}
+                        classes={classes}
+                      />
+                    </div>
+                    <ErrorMessage name="firstName" component="div" />
                   </div>
-                  <div className="form-group">
-                    <Field
-                      type="text"
-                      name="lastName"
-                      placeholder="Last Name"
-                      className={`form-control ${
-                        touched.lastName && errors.lastName ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="lastName"
-                      component="div"
-                      className={classes.invalidFeedback}
-                    />
+
+                  {/* Last Name */}
+                  <div className={`form-group ${classes.formGroup}`}>
+                    <div className={classes.inputRow}>
+                      <Field
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name"
+                        className="form-control"
+                      />
+                      <ValidationIcon
+                        touched={touched.lastName}
+                        error={errors.lastName}
+                        classes={classes}
+                      />
+                    </div>
+                    <ErrorMessage name="lastName" component="div" />
                   </div>
-                  <div className="form-group">
-                    <Field
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className={`form-control ${
-                        touched.email && errors.email ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className={classes.invalidFeedback}
-                    />
+
+                  {/* Email */}
+                  <div className={`form-group ${classes.formGroup}`}>
+                    <div className={classes.inputRow}>
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="form-control"
+                      />
+                      <ValidationIcon
+                        touched={touched.email}
+                        error={errors.email}
+                        classes={classes}
+                      />
+                    </div>
+                    <ErrorMessage name="email" component="div" />
                   </div>
-                  <div className="form-group">
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      autocomplete="new-password"
-                      className={`form-control ${
-                        touched.password && errors.password ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="password"
-                      component="div"
-                      className={classes.invalidFeedback}
-                    />
+
+                  {/* Password */}
+                  <div className={`form-group ${classes.formGroup}`}>
+                    <div className={classes.inputRow}>
+                      <Field
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        autoComplete="new-password"
+                        className="form-control"
+                      />
+                      <ValidationIcon
+                        touched={touched.password}
+                        error={errors.password}
+                        classes={classes}
+                      />
+                    </div>
+                    <ErrorMessage name="password" component="div" />
                   </div>
-                  <div className="form-group">
-                    <Field
-                      type="password"
-                      name="passwordConfirm"
-                      placeholder="Retype Password"
-                      autocomplete="new-password"
-                      className={`form-control ${
-                        touched.passwordConfirm && errors.passwordConfirm
-                          ? "is-invalid"
-                          : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      name="passwordConfirm"
-                      component="div"
-                      className={classes.invalidFeedback}
-                    />
+
+                  {/* Confirm Password */}
+                  <div className={`form-group ${classes.formGroup}`}>
+                    <div className={classes.inputRow}>
+                      <Field
+                        type="password"
+                        name="passwordConfirm"
+                        placeholder="Retype Password"
+                        autoComplete="new-password"
+                        className="form-control"
+                      />
+                      <ValidationIcon
+                        touched={touched.passwordConfirm}
+                        error={errors.passwordConfirm}
+                        classes={classes}
+                      />
+                    </div>
+                    <ErrorMessage name="passwordConfirm" component="div" />
                   </div>
 
                   <Button
@@ -212,10 +263,13 @@ const Register = props => {
                   >
                     {isSubmitting ? "Please wait..." : "Create Account"}
                   </Button>
-                  <div className="warning">
-                    <br />
-                    {errorMsg}
-                  </div>
+
+                  {errorMsg && (
+                    <div className="warning">
+                      <br />
+                      {errorMsg}
+                    </div>
+                  )}
                 </Form>
               )}
             </Formik>
@@ -224,19 +278,15 @@ const Register = props => {
       ) : (
         <>
           <h1 className={classes.heading1}>
-            Registration instructions have been sent to the email you provided.
+            Registration instructions have been sent to your email.
           </h1>
-          <h2>
-            Please allow a few minutes for the email to arrive in your inbox.
-          </h2>
+          <h2>Please allow a few minutes for delivery.</h2>
         </>
       )}
-      <br />
-      <br />
-      {submitted ? null : (
+
+      {!submitted && (
         <div className={classes.authText}>
-          Already have an account? &nbsp;{" "}
-          <Link to={{ pathname: "/login" }}>Log In</Link>
+          Already have an account? <Link to="/login">Log In</Link>
         </div>
       )}
     </ContentContainer>
