41d23630ba6452a983fc679f7dfc1b0a8ef9d4f3
John Darragh <darragh@entrotech.net>
2026-02-09 11:27:38 -0800

    Fix pacakge.json in server

--- server/app/routes/email.routes.js
+++ server/app/routes/email.routes.js
@@ -1,18 +1,7 @@
 const router = require("express").Router();
 const emailController = require("../controllers/email.controller");
 const jwtSession = require("../../middleware/jwt-session");
-const rateLimit = require("express-rate-limit");
 
-const emailRateLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 10 // limit each IP to 10 requests per windowMs for this route
-});
-
-router.post(
-  "/",
-  emailRateLimiter,
-  jwtSession.validateRoles(["isAdmin"]),
-  emailController.send
-);
+router.post("/", jwtSession.validateRoles(["isAdmin"]), emailController.send);
 
 module.exports = router;
