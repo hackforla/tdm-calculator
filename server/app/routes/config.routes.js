9e072c44fe69de8c1902115c9de77ada85ad7514
John Darragh <darragh@entrotech.net>
2026-02-09 09:20:34 -0800

    Fix pacakge.json in server

--- server/app/routes/config.routes.js
+++ server/app/routes/config.routes.js
@@ -7,13 +7,28 @@ const configWriteLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 30, // limit each IP to 30 write requests per windowMs
   standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // disable the `X-RateLimit-*` headers
+  legacyHeaders: false // disable the `X-RateLimit-*` headers
 });
 
 module.exports = router;
 
 router.get("/", configController.getAll);
 router.get("/:code", configController.getByCode);
-router.post("/", configWriteLimiter, jwtSession.validateUser, configController.post);
-router.put("/:code", configWriteLimiter, jwtSession.validateUser, configController.put);
-router.delete("/:code", configWriteLimiter, jwtSession.validateUser, configController.del);
+router.post(
+  "/",
+  configWriteLimiter,
+  jwtSession.validateUser,
+  configController.post
+);
+router.put(
+  "/:code",
+  configWriteLimiter,
+  jwtSession.validateUser,
+  configController.put
+);
+router.delete(
+  "/:code",
+  configWriteLimiter,
+  jwtSession.validateUser,
+  configController.del
+);
--- server/app/routes/email.routes.js
+++ server/app/routes/email.routes.js
@@ -4,10 +4,15 @@ const jwtSession = require("../../middleware/jwt-session");
 const rateLimit = require("express-rate-limit");
 
 const emailRateLimiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 10, // limit each IP to 10 requests per windowMs for this route
+  windowMs: 60 * 1000, // 1 minute
+  max: 10 // limit each IP to 10 requests per windowMs for this route
 });
 
-router.post("/", emailRateLimiter, jwtSession.validateRoles(["isAdmin"]), emailController.send);
+router.post(
+  "/",
+  emailRateLimiter,
+  jwtSession.validateRoles(["isAdmin"]),
+  emailController.send
+);
 
 module.exports = router;
