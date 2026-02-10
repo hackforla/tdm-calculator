eabbb64fe86f42127726eaba7605efb306ac7886
John Darragh <darragh@entrotech.net>
2026-02-10 13:56:27 -0800

    Implement rate-limiting on PUT, POST web api endpoints (#2885)

--- server/app/routes/about.routes.js
+++ server/app/routes/about.routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const jwtSession = require("../../middleware/jwt-session");
-
+const { writeLimiter } = require("../../middleware/rateLimiter");
 const aboutController = require("../controllers/about.controller");
 
 module.exports = router;
 
 router.get("/", aboutController.get);
-router.post("/", jwtSession.validateRoles(["isAdmin"]), aboutController.post);
+router.post(
+  "/",
+  writeLimiter,
+  jwtSession.validateRoles(["isAdmin"]),
+  aboutController.post
+);
