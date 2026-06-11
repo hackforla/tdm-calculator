61cacaa590bd5f9b7ff2d7bbe6016458dab1c52e
John Darragh <darragh@entrotech.net>
2026-06-11 14:14:13 -0700

    UAT env shouls show uAT badge in app header

--- client/src/helpers/Environment.js
+++ client/src/helpers/Environment.js
@@ -2,6 +2,6 @@ export const Environment =
   import.meta.env.NODE_ENV === "development" ||
   window.location.hostname.toLowerCase().includes("tdm-dev")
     ? import.meta.env.ENV || "DEV"
-    : window.location.hostname.toLowerCase().includes("tdm-uat")
+    : window.location.hostname.toLowerCase().includes("tdmusertest")
       ? "UAT"
       : "PROD";
