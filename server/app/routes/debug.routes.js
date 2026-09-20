58b7097edb2f7199211977c8757718a7f714fd39
John Darragh <darragh@entrotech.net>
2026-09-20 12:05:48 -0700

    Merge commit from fork

--- server/app/routes/debug.routes.js
+++ server/app/routes/debug.routes.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
 const debugController = require("../controllers/debug.controller");
-// const jwtSession = require("../../middleware/jwt-session");
+const jwtSession = require("../../middleware/jwt-session");
 const { writeLimiter } = require("../../middleware/rateLimiter");
 
 module.exports = router;
 
-router.get("/memory", writeLimiter, debugController.memory);
-// router.get(
-//   "/heapdump",
-//   writeLimiter,
-//   jwtSession.validateRoles(["isAdmin"]),
-//   debugController.heapDump
-// );
+router.get(
+  "/memory",
+  writeLimiter,
+  jwtSession.validateRoles(["isAdmin"]),
+  debugController.memory
+);
+
+router.get(
+  "/heapdump",
+  writeLimiter,
+  jwtSession.validateRoles(["isAdmin"]),
+  debugController.heapDump
+);
